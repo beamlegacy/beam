f270563ba8770b619580456d335b055cfc24fdc5
Sebastien Metrot <meeloo@meeloo.net>
2020-09-20 14:56:01 +0200

    remove dead code

--- Beam/Beam/UI/SearchBar.swift
+++ Beam/Beam/UI/SearchBar.swift
@@ -8,16 +8,6 @@
 import Foundation
 import SwiftUI
 
-//class QueryFormater : Formatter {
-//    override func string(for string: Any?) -> String? {
-//        return string as? String
-//    }
-//    override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?,
-//                                 for string: String,
-//                                 errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
-//
-//    }
-
 struct MainBar: View {
     @EnvironmentObject var state: BeamState
     var body: some View {
@@ -40,10 +30,6 @@ struct SearchBar: View {
                     .aspectRatio(contentMode: .fit)
             }.disabled(!state.webViewStore.webView.canGoForward).frame(alignment: .center)
             TextField("Search or create note...", text: $searchText,
-//                      onEditingChanged: { b in
-//                        print("searchText changed: \(searchText)")
-//                        state.completer.complete(query: searchText)
-//                      },
                       onCommit:  {
                         print("searchText activated: \(searchText)")
                         state.mode = .web
