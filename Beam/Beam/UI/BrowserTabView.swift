b6eaab1548014183e0ce261f9ab59ecfb21584e2
Sebastien Metrot <meeloo@meeloo.net>
2020-09-23 01:27:58 +0200

    gracefully handle tab removal

--- Beam/Beam/UI/BrowserTabView.swift
+++ Beam/Beam/UI/BrowserTabView.swift
@@ -11,13 +11,14 @@ import SwiftUI
 struct BrowserTabView: View {
     @EnvironmentObject var state: BeamState
     @ObservedObject var tab: BrowserTab
+    @State var showButton = false
     var selected: Bool
     
     static var tabColor = Color(NSColor.lightGray).opacity(0.25)
     static var selectedTabColor = Color(NSColor.gray).opacity(0.25)
     static var tabFrameColor = Color(NSColor.lightGray)
     static var selectedTabFrameColor = Color(NSColor.gray)
-
+    
     var body: some View {
         ZStack {
             GeometryReader { geometry in
@@ -31,44 +32,56 @@ struct BrowserTabView: View {
                 .stroke(selected ? Self.selectedTabFrameColor : Self.tabFrameColor)
             }
             HStack {
-                Button("x") {
-                    var tabsCopy = state.tabs
-                    tabsCopy.removeAll(where: { t -> Bool in
-                        t.id == tab.id
-                    })
-                    state.tabs = tabsCopy
-                }
+                Button("X") {
+                    for (i,t) in state.tabs.enumerated() {
+                        if t.id == tab.id {
+                            if i > 0 {
+                                state.currentTab = state.tabs[i - 1]
+                            } else if state.tabs.count > 1 {
+                                state.currentTab = state.tabs[i + 1]
+                            } else {
+                                state.currentTab = BrowserTab()
+                                state.mode = .note
+                            }
+                            state.tabs.remove(at: i)
+                            break
+                        }
+                    }
+                }.opacity(showButton ? 1 : 0)
+                .onHover(perform: { v in
+                    showButton = v
+                })
                 .buttonStyle(BorderlessButtonStyle())
                 .padding(.leading, 4)
-                
-                Text(tab.title)
-                    .padding(.top, 2)
-                    .padding([.leading, .trailing], 10)
-                    .frame(minWidth: 50, maxWidth: .infinity, minHeight: 20, maxHeight: 20, alignment: .leading)
-                    .font(selected ? .system(size: 12, weight: .bold) : .system(size: 12, weight: .regular))
-            }
-        }.frame(minWidth: 50, maxWidth: .infinity, minHeight: 20, maxHeight: 20, alignment: .leading)
-        .padding([.trailing], 1)
-        .padding([.top], 2)
 
-    }
+            Text(tab.title)
+            .padding(.top, 2)
+            .padding([.trailing], 10)
+            .frame(minWidth: 50, maxWidth: .infinity, minHeight: 20, maxHeight: 20, alignment: .leading)
+            .font(selected ? .system(size: 12, weight: .bold) : .system(size: 12, weight: .regular))
+        }
+    }.frame(minWidth: 50, maxWidth: .infinity, minHeight: 20, maxHeight: 20, alignment: .leading)
+    .padding([.trailing], 1)
+    .padding([.top], 2)
     
-    func buildTabDecoration(_ path: inout Path, width: Int, height: Int) {
-        let radius = Int(2)
-        
-        path.move(to: CGPoint(x: 0, y: height))
-        path.addLine(to: CGPoint(x: 0, y: radius))
-        path.addArc(center: CGPoint(x: radius, y: radius),
-                    radius: CGFloat(radius),
-                    startAngle: Angle(degrees: 180), endAngle: Angle(degrees: -90), clockwise: false)
-        path.addLine(to: CGPoint(x: width - radius, y: 0))
-        path.addArc(center: CGPoint(x: width - radius, y: radius),
-                    radius: CGFloat(radius),
-                    startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
-        path.addLine(to: CGPoint(x: width, y: height))
-        path.closeSubpath()
+}
 
-    }
+func buildTabDecoration(_ path: inout Path, width: Int, height: Int) {
+    let radius = Int(2)
+    
+    path.move(to: CGPoint(x: 0, y: height))
+    path.addLine(to: CGPoint(x: 0, y: radius))
+    path.addArc(center: CGPoint(x: radius, y: radius),
+                radius: CGFloat(radius),
+                startAngle: Angle(degrees: 180), endAngle: Angle(degrees: -90), clockwise: false)
+    path.addLine(to: CGPoint(x: width - radius, y: 0))
+    path.addArc(center: CGPoint(x: width - radius, y: radius),
+                radius: CGFloat(radius),
+                startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+    path.addLine(to: CGPoint(x: width, y: height))
+    path.closeSubpath()
+    
+}
 }
 
 struct BrowserTabView_Previews: PreviewProvider {
