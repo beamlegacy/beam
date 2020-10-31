8ac2ecc4dad3c91290db0d5f64850306bce6ed21
Sebastien Metrot <meeloo@meeloo.net>
2020-10-31 14:02:01 +0100

    moved BeamData to its own file

---
+++ Beam/Beam/DataModel/BeamData.swift
@@ -0,0 +1,66 @@
+//
+//  BeamData.swift
+//  Beam
+//
+//  Created by Sebastien Metrot on 31/10/2020.
+//
+
+import Foundation
+
+class BeamData: ObservableObject {
+    var _todaysNote: Note?
+    var todaysNote: Note {
+        if let note = _todaysNote, note.title == todaysName {
+            return note
+        }
+
+        setupJournal()
+        return _todaysNote!
+    }
+    @Published var journal: [Note] = []
+
+    var searchKit: SearchKit
+
+    init() {
+        let paths = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true)
+        if let applicationSupportDirectory = paths.first {
+            let indexPath = URL(fileURLWithPath: applicationSupportDirectory + "/index.sk")
+            searchKit = SearchKit(indexPath)
+        } else {
+            searchKit = SearchKit(URL(fileURLWithPath: "~/Application Data/BeamApp/index.sk"))
+        }
+    }
+
+    var todaysName: String {
+        let fmt = DateFormatter()
+        let today = Date()
+        fmt.dateStyle = .long
+        fmt.doesRelativeDateFormatting = false
+        fmt.timeStyle = .none
+        return fmt.string(from: today)
+    }
+
+    func setupJournal() {
+        if let note = Note.fetchWithTitle(CoreDataManager.shared.mainContext, todaysName) {
+            print("Today's note loaded:\n\(note)\n")
+            _todaysNote = note
+        } else {
+            let note = Note.createNote(CoreDataManager.shared.mainContext, todaysName)
+            note.type = NoteType.journal.rawValue
+            let bullet = note.createBullet(CoreDataManager.shared.mainContext, content: "")
+            note.addToBullets(bullet)
+            _todaysNote = note
+            print("Today's note created:\n\(note)\n")
+
+            CoreDataManager.shared.save()
+        }
+
+        updateJournal()
+    }
+
+    func updateJournal() {
+        journal = Note.fetchAllWithType(CoreDataManager.shared.mainContext, .journal)
+        print("Journal updated:\n\(journal)\n")
+    }
+}
+
