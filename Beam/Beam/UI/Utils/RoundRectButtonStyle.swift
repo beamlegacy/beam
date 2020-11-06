0a3ee4605502afdcee8916d135de6376af9a4741
Sebastien Metrot <meeloo@meeloo.net>
2020-11-06 14:16:04 +0100

    hack around Button to have the correct colors

--- Beam/Beam/UI/Utils/RoundRectButtonStyle.swift
+++ Beam/Beam/UI/Utils/RoundRectButtonStyle.swift
@@ -11,22 +11,63 @@ import SwiftUI
 struct RoundRectButtonStyle: PrimitiveButtonStyle {
     private var _cornerRadius = CGFloat(7)
 
-    @Environment(\.isEnabled) private var isEnabled: Bool
+    private enum ButtonGestureState {
+        case inactive
+        case pressing
+        case outOfBounds
+
+        var isPressed: Bool {
+            switch self {
+            case .pressing:
+                return true
+            default:
+                return false
+            }
+        }
+    }
+
+    @GestureState private var dragState = ButtonGestureState.inactive
+
+    @Environment(\.isEnabled) var isEnabled
     @State var isHover = false
-    var foregroundColor: Color {
+    @State var isPressed = false
+    func foregroundColor(isPressed: Bool) -> Color {
         guard isEnabled else { return Color(.displayP3, white: 1, opacity: 0) }
+        if isPressed {
+            return Color("ToolbarButtonBackgroundOnColor")
+        }
         return isHover ? Color("ToolbarButtonBackgroundHoverColor") : Color.init(.displayP3, white: 1, opacity: 0)
     }
-    public func makeBody(configuration: BorderedButtonStyle.Configuration) -> some View {
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        let dragGesture = DragGesture(minimumDistance: 0)
+            .updating($dragState, body: { _, state, _ in
+                state = isHover ? .pressing : .outOfBounds
+            })
+            .onChanged({ _ in
+                withAnimation(.easeInOut(duration: 0.16)) {
+                    self.isPressed = self.dragState.isPressed
+                }
+            })
+            .onEnded { _ in
+                print("onEnded (\(isPressed))")
+                if self.isPressed {
+                    configuration.trigger()
+                }
+
+                withAnimation(.easeInOut(duration: 0.16)) {
+                    self.isPressed = self.dragState.isPressed
+                }
+            }
+
         return ZStack {
-            RoundedRectangle(cornerRadius: _cornerRadius).foregroundColor(foregroundColor).frame(width: 33, height: 28, alignment: .center)
+            RoundedRectangle(cornerRadius: _cornerRadius).foregroundColor(foregroundColor(isPressed: isPressed))
+                .frame(width: 33, height: 28, alignment: .center)
+
             configuration.label.foregroundColor(Color(isEnabled ? "ToolbarButtonIconColor" : "ToolbarButtonIconDisabledColor"))
-        }
-        .onTapGesture(count: 1) {
-            configuration.trigger()
         }
         .onHover { h in
             isHover = h && isEnabled
         }
+        .highPriorityGesture(dragGesture, including: .gesture)
     }
 }
