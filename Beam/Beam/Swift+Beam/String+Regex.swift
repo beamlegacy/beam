2f7293aea2b19c1552fdabb438b112f23bf8c3a9
Sebastien Metrot <meeloo@meeloo.net>
2020-11-16 16:02:07 +0100

    fix mayBeURL for cases

--- Beam/Beam/Swift+Beam/String+Regex.swift
+++ Beam/Beam/Swift+Beam/String+Regex.swift
@@ -52,7 +52,7 @@ public extension String {
     }
 
     var maybeURL: Bool {
-        return self.matches(withRegex: "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$")
+        return self.lowercased().matches(withRegex: "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$")
     }
 
 }
