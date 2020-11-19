402364fb0d647395c21aba04e6f6c1bdfcc332c4
Sebastien Metrot <seb@beamapp.co>
2020-11-19 16:59:00 +0100

    comment out safe areas test that doesn't work yet

--- Beam/Beam/DataModel/BrowserTab.swift
+++ Beam/Beam/DataModel/BrowserTab.swift
@@ -12,9 +12,9 @@ import WebKit
 import FavIcon
 
 class FullScreenWKWebView: WKWebView {
-    override var safeAreaInsets: NSEdgeInsets {
-        return NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
+//    override var safeAreaInsets: NSEdgeInsets {
+//        return NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    }
 }
 
 class BrowserTab: NSObject, ObservableObject, Identifiable, WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler {
