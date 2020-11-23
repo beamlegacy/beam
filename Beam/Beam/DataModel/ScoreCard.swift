c9b0180584f7ee370440c9af3f0ba5e798f548bc
Sebastien Metrot <seb@beamapp.co>
2020-11-23 18:40:32 +0100

    tentative ScoreCard

---
+++ Beam/Beam/DataModel/ScoreCard.swift
@@ -0,0 +1,58 @@
+//
+//  ScoreCard.swift
+//  Beam
+//
+//  Created by Sebastien Metrot on 23/11/2020.
+//
+
+import Foundation
+
+class ScoreCard {
+    var score: Float {
+            readingTimeScore
+            + textSelectionsScore
+            + scrollRatioScore
+            + searchIndexScore
+            + outboundsScore
+            + densityScore
+    }
+
+    var readingTime: CFTimeInterval = 0 //< how long did the user spent reading this page
+    var textSelections: Int = 0 //< how many chunks of text were selected by the user
+    var scrollRatio: Int = 0 //< how much of the page was seen by the user ([0, 1])
+    var searchIndex: Int = 0 //< how many clicks on the search results page before this tab was created
+    var outbounds: Int = 0 //< how many links were followed from this page
+    var textAmount: Int = 0 //< amount of text (= the caracter counts from readability)
+    var area: Float = 0 //< the page area in points
+    var inbounds: Int = 0 //< Number of pages that reference this page
+    var videoTotalDuration: CFTimeInterval = 0 //< The number of seconds of the video objects cumulated
+    var videoReadingDuration: CFTimeInterval = 0 //< The number of seconds spent viewing video objects
+
+    var readingTimeScore: Float {
+        Float(readingTime)
+    }
+
+    var textSelectionsScore: Float {
+        Float(textSelections)
+    }
+
+    var scrollRatioScore: Float {
+        Float(scrollRatio)
+    }
+
+    var searchIndexScore: Float {
+        Float(searchIndex)
+    }
+
+    var outboundsScore: Float {
+        Float(outbounds)
+    }
+
+    var densityScore: Float {
+        area > 0 ? Float(textAmount) / area : 0
+    }
+
+    var videoScore: Float {
+        videoTotalDuration > 0 ? Float(videoReadingDuration / videoTotalDuration) : 0
+    }
+}
