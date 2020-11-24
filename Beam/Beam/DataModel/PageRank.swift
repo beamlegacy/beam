626967f2b1bad03f325da08d4cb1fffb10eaba68
Sebastien Metrot <seb@beamapp.co>
2020-11-24 15:29:39 +0100

    start small page ranking structure

--- Beam/Beam/DataModel/PageRank.swift
+++ Beam/Beam/DataModel/PageRank.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import SwiftSoup
+
+class PageRank: Codable {
+    class Page: Codable {
+        var inbound = Set<String>()
+        var outbound = Set<String>()
+
+        var pageRange = Float(0)
+    }
+
+    var pages = [String: Page]()
+
+    func updatePage(url: String, contents: String) {
+        do {
+            //print("html -> \(html)")
+            let doc = try SwiftSoup.parseBodyFragment(contents)
+            let els: Elements = try doc.select("a")
+
+            let page = Page()
+
+            // capture all the links containted in the page:
+            for element: Element in els.array() {
+                let href = try element.attr("href")
+                page.outbound.insert(href)
+            }
+
+            if let oldPage = pages[url] {
+                let common = oldPage.outbound.intersection(page.outbound)
+                let toDelete = oldPage.outbound.subtracting(common)
+                let toAdd = page.outbound.subtracting(common)
+
+                for linkToUpdate in toDelete {
+                    pages[linkToUpdate]?.inbound.remove(url)
+                }
+
+                for linkToUpdate in toAdd {
+                    if let page = pages[linkToUpdate] {
+                        page.inbound.insert(url)
+                    } else {
+                        let p = Page()
+                        p.inbound.insert(url)
+                        pages[linkToUpdate] = p
+                    }
+                    pages[linkToUpdate]?.inbound.remove(url)
+                }
+
+                page.inbound = oldPage.inbound
+            }
+
+            pages[url] = page
+
+        } catch Exception.Error(let type, let message) {
+            print("PageRank \(type): \(message)")
+        } catch {
+            print("PageRank: error")
+        }
+    }
+
+    func computePageRanks() {
+        for pageIterator in pages {
+            let url = pageIterator.key
+            let page = pageIterator.value
+
+            
+        }
+    }
+}
