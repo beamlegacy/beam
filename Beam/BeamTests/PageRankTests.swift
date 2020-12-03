a621ad3f580ab86f930a5a192947c1a19b4ea548
Sebastien Metrot <seb@beamapp.co>
2020-12-03 15:32:20 +0100

    adde empty tests

---
+++ Beam/BeamTests/PageRankTests.swift
@@ -0,0 +1,16 @@
+//
+//  PageRankTests.swift
+//  BeamTests
+//
+//  Created by Sebastien Metrot on 03/12/2020.
+//
+
+import Foundation
+import XCTest
+@testable import Beam
+
+class PageRangeTests: CoreDataTests {
+
+    func testInit() throws {
+    }
+}
