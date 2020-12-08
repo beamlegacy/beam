ea0c915dc235ec9264a8190fddc718c83e1ca7f0
Ravichandrane Rajendran <ravi@beamapp.cp>
2020-12-08 16:04:00 +0100

    refactor(image): add prefix

--- Beam/Beam/UI/TextEditor/TextNode.swift
+++ Beam/Beam/UI/TextEditor/TextNode.swift
@@ -378,12 +378,12 @@ public class TextNode: NSObject, CALayerDelegate {
     }
 
     func drawDisclosure(at point: NSPoint, in context: CGContext) {
-        let symbol = open ? "arrow_down" : "arrow_right"
+        let symbol = open ? "editor-arrow_down" : "editor-arrow_right"
         drawImage(named: symbol, at: point, in: context)
     }
 
     func drawBulletPoint(at point: NSPoint, in context: CGContext) {
-        drawImage(named: "bullet", at: point, in: context, CGRect(x: 0, y: 0, width: 12, height: 12))
+        drawImage(named: "editor-bullet", at: point, in: context, CGRect(x: 0, y: 0, width: 12, height: 12))
     }
 
     func drawDebug(in context: CGContext) {
