26e0473a7927463cd3388c472dbfdc86b6518fe3
Ravi <ravi@beamapp.co>
2020-12-16 10:40:35 +0100

    fix(crash): issue with line

--- Beam/Classes/Components/TextEditor/TextNode.swift
+++ Beam/Classes/Components/TextEditor/TextNode.swift
@@ -557,7 +557,7 @@ public class TextNode: NSObject, CALayerDelegate {
 
     func drawCursor(in context: CGContext) {
         // Draw fake cursor if the text is empty
-        if text.isEmpty {
+        if text.isEmpty || layout!.lines.count == 0 {
             guard editor.hasFocus, editor.blinkPhase else { return }
 
             let f = AttributedStringVisitor.font(fontSize)
