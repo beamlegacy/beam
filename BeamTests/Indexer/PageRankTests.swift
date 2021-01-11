c387b9aedf0a4cd36966d6867b3fcd9f6a33a117
Sebastien Metrot <seb@beamapp.co>
2021-01-11 18:11:56 +0100

    remove embedding test that was actually just a stupid test with no direct relation to Beam's features

--- BeamTests/Indexer/PageRankTests.swift
+++ BeamTests/Indexer/PageRankTests.swift
@@ -43,34 +43,6 @@ class PageRankTests: XCTestCase {
         XCTAssertEqual(total, 1.0, accuracy: 0.0015)
     }
 
-    func testEmbeddings() {
-        let embeddingFrench = NLEmbedding.wordEmbedding(for: .french)
-        let embeddingEnglish = NLEmbedding.wordEmbedding(for: .english)
-
-        let frenchWord = "humain"
-        let englishWord = "human"
-
-        guard let frenchVector = embeddingFrench?.vector(for: frenchWord) else { fatalError() }
-        guard let englishVector = embeddingEnglish?.vector(for: englishWord) else { fatalError() }
-
-        Logger.shared.logInfo("French embedding '\(frenchWord) -> \(frenchVector)", category: .general)
-        Logger.shared.logInfo("English embedding '\(englishWord) -> \(englishVector)", category: .general)
-
-//        let stride = vDSP_Stride(1)
-
-        XCTAssert(frenchVector.count == englishVector.count)
-//        var result = frenchVector
-
-//        vDSP_vdistD(frenchVector, stride,
-//                   englishVector, stride,
-//                   &result, stride,
-//                   vDSP_Length(result.count))
-
-        let result = vDSP.distanceSquared(frenchVector, englishVector)
-
-        Logger.shared.logInfo(" -> distance '\(result)", category: .general)
-    }
-
     var index = Index()
 
     struct SearchSource: Codable {
