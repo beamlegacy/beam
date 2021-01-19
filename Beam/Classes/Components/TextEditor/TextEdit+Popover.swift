7b38601b58c55dd48a7efc8d1eb83f55ae2ad2ef
Ravi <ravi@beamapp.co>
2021-01-19 12:06:25 +0100

    refactor(variable): remove variable

--- Beam/Classes/Components/TextEditor/TextEdit+Popover.swift
+++ Beam/Classes/Components/TextEditor/TextEdit+Popover.swift
@@ -12,7 +12,6 @@ extension BeamTextEdit {
     // MARK: - Properties
     private static var xPos: CGFloat = 0
     private static var yPos: CGFloat = 0
-    private static var popoverType: PopoverType = .down
 
     internal func initPopover() {
         guard let node = node as? TextNode else { return }
