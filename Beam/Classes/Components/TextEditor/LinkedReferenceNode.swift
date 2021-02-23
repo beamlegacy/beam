02a28f543bd5f293943f4e41e9727e57f0924911
Ravi <ravi@beamapp.co>
2021-02-23 14:00:14 +0100

    rollback update children fixe

--- Beam/Classes/Components/TextEditor/LinkedReferenceNode.swift
+++ Beam/Classes/Components/TextEditor/LinkedReferenceNode.swift
@@ -132,7 +132,7 @@ class LinkedReferenceNode: TextNode {
                 })
 
                 self.invalidateRendering()
-                updateChildrenVisibility(visible)
+                updateChildrenVisibility(visible && open)
         }.store(in: &scope)
     }
 
