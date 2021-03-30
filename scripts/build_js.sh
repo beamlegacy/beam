d000564227f2b75ffddb308cf66ea9aaeed619df
Jérôme Beau <jerome@beamapp.co>
2021-03-30 15:11:26 +0200

    Use yarn

--- scripts/build_js.sh
+++ scripts/build_js.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/opt/homebrew/bin/npm --verbose --prefix ${PROJECT_DIR}/Beam/Classes/Helpers/Utils/Web run build
+projectDir=${PROJECT_DIR:-..}
+echo "projectDir=""${projectDir}"""
+packageManager=$(which yarn)
+echo "packageManager=""${packageManager}"""
+${packageManager} --verbose --cwd "${projectDir}"/Beam/Classes/Helpers/Utils/Web run build
