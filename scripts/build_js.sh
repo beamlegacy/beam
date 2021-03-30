cfad6df046c1f24c29a5ca1f14bc8110a0c73346
Jérôme Beau <jerome@beamapp.co>
2021-03-30 19:29:03 +0200

    simplified js build command

--- scripts/build_js.sh
+++ scripts/build_js.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-projectDir=${PROJECT_DIR:-..}
-echo "projectDir=""${projectDir}"""
-packageManager=$(which yarn)
-echo "packageManager=""${packageManager}"""
-${packageManager} --verbose --cwd "${projectDir}"/Beam/Classes/Helpers/Utils/Web run build
+yarn --cwd "${PROJECT_DIR:-..}"/Beam/Classes/Helpers/Utils/Web run build
