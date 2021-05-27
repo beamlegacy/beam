76f95822e6302e46b6a98724feb3ae13aac17312
Gil Katz <gil@beamapp.co>
2021-05-27 08:42:53 +0000

    BE-752: First version of spectral clustering over navigation data

---
+++ Beam/Clustering/Clustering/Clustering.h
@@ -0,0 +1,18 @@
+//
+//  Clustering.h
+//  Clustering
+//
+//  Created by Gil Katz on 17/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Clustering.
+FOUNDATION_EXPORT double ClusteringVersionNumber;
+
+//! Project version string for Clustering.
+FOUNDATION_EXPORT const unsigned char ClusteringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clustering/PublicHeader.h>
+
+
