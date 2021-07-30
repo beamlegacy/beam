70f99ca7a65613b5a842695824a4f8134260cbc9
Stef Kors <stef@beamapp.co>
2021-07-30 09:16:57 +0000

    Stef/BE-1507 Fix Google Sheets error by modifying the UserAgent string for "docs.google.com"

--- Beam/Configuration/Constants.swift
+++ Beam/Configuration/Constants.swift
@@ -14,4 +14,5 @@ enum Constants {
         return version.majorVersion >= 11 || (version.majorVersion == 10 && version.minorVersion >= 16)
     }()
 
+    static var SafariUserAgent: String = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"
 }
