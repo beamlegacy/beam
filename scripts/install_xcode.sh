6ddb8e276900c76cab5c202b4ce15c34a1fed996
Fabien Penso <git@pen.so>
2021-10-01 08:46:26 +0000

    Enforce using the proper xcode version

--- scripts/install_xcode.sh
+++ scripts/install_xcode.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 OSX_VERSION=`sw_vers -productVersion | cut -d'.' -f1`
 HOSTNAME=`hostname | cut -d'.' -f1 | tr '[a-z]' '[A-Z]'`
 
@@ -15,10 +14,13 @@ xcversion update
 if [[ -n ${!XCODE_HOSTNAME_VERSION} ]]; then
 	echo "Installing Xcode ${!XCODE_HOSTNAME_VERSION} based on XCODE_VERSION_FOR_HOST_${HOSTNAME}"
 	xcversion install ${!XCODE_HOSTNAME_VERSION}
+	echo ${!XCODE_HOSTNAME_VERSION} > .xcode_version
 elif [[ -n ${!XCODE_OSX_VERSION} ]]; then
 	echo "Installing Xcode ${!XCODE_OSX_VERSION} based on XCODE_VERSION_FOR_MACOS_${OSX_VERSION}. XCODE_VERSION_FOR_HOST_${HOSTNAME} not found."
 	xcversion install ${!XCODE_OSX_VERSION}
+	echo ${!XCODE_OSX_VERSION} > .xcode_version
 else
 	echo "Installing Xcode ${XCODE_VERSION} based on XCODE_VERSION. XCODE_VERSION_FOR_MACOS_${OSX_VERSION} nor XCODE_VERSION_FOR_HOST_${HOSTNAME} exist."
 	xcversion install ${XCODE_VERSION}
+	echo ${XCODE_VERSION} > .xcode_version
 fi
