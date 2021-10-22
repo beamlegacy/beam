db297ded6520251efadd15bed84aec5d63177cb7
Florian Mari <florian@beamapp.co>
2021-10-22 09:39:21 +0000

    Fix flaky UI test on Omnibar

--- Beam/Classes/Components/Autocomplete/AutocompleteManager.swift
+++ Beam/Classes/Components/Autocomplete/AutocompleteManager.swift
@@ -12,8 +12,8 @@ import SwiftUI
 
 // BeamState Autocomplete management
 class AutocompleteManager: ObservableObject {
-    public static let noteFrecencyParamKey: FrecencyParamKey = .note30d0
-    public static let urlFrecencyParamKey: FrecencyParamKey = .webReadingTime30d0
+    static let noteFrecencyParamKey: FrecencyParamKey = .note30d0
+    static let urlFrecencyParamKey: FrecencyParamKey = .webReadingTime30d0
 
     @Published var searchQuery: String = ""
 
