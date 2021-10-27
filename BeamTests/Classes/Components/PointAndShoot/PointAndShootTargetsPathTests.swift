3a00fd3f3bd1f9b02c529281c4c9a8a30f966616
Remi Santos <remi@beamapp.co>
2021-10-27 08:15:45 +0000

    BE-2241 Improve PnS path calculation

--- BeamTests/Classes/Components/PointAndShoot/PointAndShootTargetsPathTests.swift
+++ BeamTests/Classes/Components/PointAndShoot/PointAndShootTargetsPathTests.swift
@@ -1,5 +1,5 @@
 //
-//  CGPath+UnionTest.swift
+//  PointAndShootTargetsPathTests.swift
 //  BeamTests
 //
 //  Created by Stef Kors on 02/08/2021.
@@ -12,7 +12,8 @@ import Nimble
 @testable import Beam
 @testable import BeamCore
 
-class CGPath_UnionTest: PointAndShootTest {
+class PointAndShootTargetsPathTests: PointAndShootTest {
+
     func helperCreateFakeTargets(_ count: Int, _ offset: Int = 0) -> [PointAndShoot.Target] {
         var targets: [PointAndShoot.Target] = []
 
@@ -45,7 +46,7 @@ class CGPath_UnionTest: PointAndShootTest {
         let targets = self.helperCreateFakeTargets(10)
         let newTargets = self.helperCreateFakeTargets(10, 5)
         let id = UUID().uuidString
-        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url())
+        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url(), shapeCache: nil)
         let beforeGroupPath = group.groupPath
         self.measure {
             group.updateTargets(id, newTargets)
@@ -58,7 +59,7 @@ class CGPath_UnionTest: PointAndShootTest {
         let targets = self.helperCreateFakeTargets(400)
         let newTargets = self.helperCreateFakeTargets(400, 5)
         let id = UUID().uuidString
-        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url())
+        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url(), shapeCache: nil)
         let beforeGroupPath = group.groupPath
         self.measure {
             group.updateTargets(id, newTargets)
@@ -71,13 +72,38 @@ class CGPath_UnionTest: PointAndShootTest {
         let targets = self.helperCreateFakeTargets(40000)
         let newTargets = self.helperCreateFakeTargets(40000, 5)
         let id = UUID().uuidString
-        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url())
+        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url(), shapeCache: nil)
         let beforeGroupPath = group.groupPath
         self.measure {
             group.updateTargets(id, newTargets)
         }
         // Assert the groupPath is updated
         XCTAssertNotEqual(beforeGroupPath, group.groupPath)
+    }
+
+    func testShapeCacheWith40000() throws {
+        let shapeCache = PointAndShoot.PnSTargetsShapeCache()
+        let targets = self.helperCreateFakeTargets(40000)
+        let id = UUID().uuidString
+        var group = PointAndShoot.ShootGroup(id, targets, "placeholder string", faker.internet.url(), shapeCache: shapeCache)
+
+        // Translating the targets should produce a similar path but with different origin
+        let beforeShiftPath = group.groupPath
+        let shift: CGFloat = 100 // simulate 100pt scroll
+        let shiftedTargets = targets.map { t -> PointAndShoot.Target in
+            var target = t
+            target.rect.origin.x += shift
+            target.rect.origin.y += shift
+            return target
+        }
+        self.measure {
+            group.updateTargets(id, shiftedTargets)
+        }
+        XCTAssertNotEqual(beforeShiftPath, group.groupPath)
+        XCTAssertEqual(beforeShiftPath.boundingBox.origin.x + shift, group.groupPath.boundingBox.origin.x)
+        XCTAssertEqual(beforeShiftPath.boundingBox.origin.y + shift, group.groupPath.boundingBox.origin.y)
+        XCTAssertEqual(beforeShiftPath.boundingBox.width, group.groupPath.boundingBox.width)
+        XCTAssertEqual(beforeShiftPath.boundingBox.height, group.groupPath.boundingBox.height)
     }
 
 }
