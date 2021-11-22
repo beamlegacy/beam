52c70fda39c496b30af42f4551296bbe5c10e2bb
Remi Santos <remi@beamapp.co>
2021-11-22 10:22:53 +0000

    BE-2343 Remove Creating a card appears in Journal

--- BeamUITests/E2ETests/BiDi/ReferencesTests.swift
+++ BeamUITests/E2ETests/BiDi/ReferencesTests.swift
@@ -23,18 +23,18 @@ class ReferencesTests: BaseTest {
         testRailPrint("Then I reference card 2 to card 1")
         cardView.createReference(cardName1)
         journalView.openRecentCardByName(cardName1)
-        XCTAssertEqual(cardView.getLinksNamesNumber(), 1) //Link ONLY
-        XCTAssertEqual(cardView.getLinksContentNumber(), 1)
+        XCTAssertEqual(cardView.getLinksNamesNumber(), 0) //Link ONLY
+        XCTAssertEqual(cardView.getLinksContentNumber(), 0)
         
         cardView.expandReferenceSection()
-        XCTAssertEqual(cardView.getLinksNamesNumber(), 2) // Link and Reference
-        XCTAssertEqual(cardView.getLinksContentNumber(), 2)
-        XCTAssertEqual(cardView.getLinkNameByIndex(1), cardName2)
-        XCTAssertEqual(cardView.getLinkContentByIndex(1), cardName1)
+        XCTAssertEqual(cardView.getLinksNamesNumber(), 1) // Link and Reference
+        XCTAssertEqual(cardView.getLinksContentNumber(), 1)
+        XCTAssertEqual(cardView.getLinkNameByIndex(0), cardName2)
+        XCTAssertEqual(cardView.getLinkContentByIndex(0), cardName1)
         
         testRailPrint("Then I can navigate to a card by Reference to a source card")
-        cardView.openLinkByIndex(1)
-        XCTAssertEqual(cardView.getNumberOfVisibleNotes(), 3)
+        cardView.openLinkByIndex(0)
+        XCTAssertEqual(cardView.getNumberOfVisibleNotes(), 2)
         XCTAssertEqual(cardView.getCardNoteValueByIndex(0), cardName1)
     }
 }
