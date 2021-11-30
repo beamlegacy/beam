c35903a5ab7f79698ff7d56da73c378f8e2fa22a
Andrii Vasyliev <andrii@beamapp.co>
2021-11-30 09:45:58 +0000

    XCUITest for Expand and collapse embed image

---
+++ BeamUITests/E2ETests/Cards/CardViewEmbedsTests.swift
@@ -0,0 +1,55 @@
+//
+//  CardViewEmbedsTests.swift
+//  BeamUITests
+//
+//  Created by Andrii on 26/11/2021.
+//
+
+import Foundation
+import XCTest
+
+class CardViewEmbedsTests: BaseTest {
+    
+    func testEmbedsCollapseExpandIcons() {
+        let toLinkTitle = "to Link"
+        let toImageTitle = "to Image"
+        let pnsView = PnSTestView()
+        let webView = WebTestView()
+        
+        testRailPrint("When I add image to a card")
+        BeamUITestsHelper(launchApp().app).openTestPage(page: .page4)
+        let imageItemToAdd = pnsView.image("forest")
+        pnsView.addToTodayCard(imageItemToAdd)
+        
+        testRailPrint("Then I see collapse button")
+        let cardView = webView.openDestinationCard()
+        let expandButton = cardView.getNoteExpandButtonByIndex(noteIndex: 0)
+        XCTAssertEqual(cardView.getNotesExpandButtonsCount(), 1)
+        XCTAssertEqual(expandButton.title, toLinkTitle)
+        
+        testRailPrint("When I click collapse button")
+        let sizeBeforeCollapse = cardView.getImageNoteByIndex(noteIndex: 0).getSize()
+        cardView.clickNoteExpandButtonByIndex(noteIndex: 0)
+        
+        testRailPrint("Then image node is collapsed")
+        XCTAssertEqual(expandButton.title, toImageTitle)
+        let sizeAfterCollapse = cardView.getImageNoteByIndex(noteIndex: 0).getSize()
+        
+        testRailPrint("Then element width and height is changed accordingly")
+        XCTAssertEqual(sizeBeforeCollapse.width, sizeAfterCollapse.width)
+        XCTAssertTrue(sizeBeforeCollapse.height > sizeAfterCollapse.height * 5 && sizeBeforeCollapse.height < sizeAfterCollapse.height * 7)
+        
+        /*Blocked by https://linear.app/beamapp/issue/BE-2559/expand-icon-overlaps-image-link
+        testRailPrint("Then TBD expand")
+        cardView.clickNoteExpandButtonByIndex(noteIndex: 0)
+        XCTAssertEqual(cardView.getNotesExpandButtonsCount(), 1)
+        XCTAssertEqual(cardView.getNoteExpandButtonByIndex(noteIndex: 0).title, toLinkTitle)
+        
+        testRailPrint("Then element width and height is changed accordingly")
+        let sizeAfterExpand = cardView.getImageNoteByIndex(noteIndex: 0).getSize()
+        XCTAssertEqual(sizeBeforeCollapse.width, sizeAfterExpand.width)
+        XCTAssertEqual(sizeBeforeCollapse.height, sizeAfterExpand.height)
+        */
+    }
+    
+}
