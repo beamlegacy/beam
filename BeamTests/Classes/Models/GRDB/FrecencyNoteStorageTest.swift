c1eb04d9a991cd0e639445e7993c5fafdcc7224d
Paul Lefkopoulos <paul@beamapp.co>
2022-01-17 17:07:48 +0000

    BE 2961 Fix noteFrecencyStoreTest flakiness

--- BeamTests/Classes/Models/GRDB/FrecencyNoteStorageTest.swift
+++ BeamTests/Classes/Models/GRDB/FrecencyNoteStorageTest.swift
@@ -12,7 +12,9 @@ import XCTest
 class FrecencyNoteStorageTest: XCTestCase {
 
     func testSaveFetch() throws {
-        //Checks that createdAt is written once at creation and that updated at each create/Update
+        //Checks that createdAt is written once at creation and then updated at each create/Update
+        BeamDate.freeze("2001-01-01T00:00:00+000")
+
         let db = GRDBDatabase.empty()
         let storage = GRDBNoteFrecencyStorage(db: db)
 
@@ -22,6 +24,8 @@ class FrecencyNoteStorageTest: XCTestCase {
         let createdAt0 = try XCTUnwrap(fetchedRecord?.createdAt)
         let updatedAt0 = try XCTUnwrap(fetchedRecord?.updatedAt)
         
+        BeamDate.travel(1)
+
         try storage.save(score: score, paramKey: .note30d0)
         fetchedRecord = try db.fetchOneFrecencyNote(noteId: score.id, paramKey: .note30d0)
         let createdAt1 = try XCTUnwrap(fetchedRecord?.createdAt)
@@ -29,5 +33,6 @@ class FrecencyNoteStorageTest: XCTestCase {
         XCTAssertEqual(createdAt0, createdAt1)
         XCTAssert(updatedAt0 < updatedAt1)
 
+        BeamDate.reset()
     }
 }
