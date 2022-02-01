51fafc5a1f0d4fb6c78c34f0422f72e27deca9d4
Jean-Louis Darmon <luis@beamapp.co>
2022-02-01 15:46:41 +0000

    BE-3117/BE-2920 Image Embeds spacing and position tweak

--- Beam/Classes/Components/BeamTextEdit/TextNode.swift
+++ Beam/Classes/Components/BeamTextEdit/TextNode.swift
@@ -137,6 +137,7 @@ public class TextNode: ElementNode {
         let textFrame = emptyTextFrame ?? textFrame
         return textFrame?.lines.first?.bounds.height ?? CGFloat(fontSize * interlineFactor)
     }
+
     override var firstLineBaseline: CGFloat {
         let textFrame = emptyTextFrame ?? self.textFrame
         if let firstLine = textFrame?.lines.first {
