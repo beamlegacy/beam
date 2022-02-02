7af43672c438cbbf294a007f3e6ac4d9e745b2e7
Jean-Louis Darmon <luis@beamapp.co>
2022-02-02 19:20:16 +0000

    BE-2920 Image Node Selection Fix & Blend Mode

--- Beam/Classes/Components/BeamTextEdit/DividerNode.swift
+++ Beam/Classes/Components/BeamTextEdit/DividerNode.swift
@@ -84,7 +84,7 @@ public class DividerNode: ElementNode {
         let padding = contentsPadding
         var size = visibleSize
         size.width = selectionLayerWidth
-        let origin = CGPoint(x: -padding.left + Self.indentLayerPosX, y: padding.top - (size.height / 2))
+        let origin = CGPoint(x: -padding.left + Self.indentLayerPositionX, y: padding.top - (size.height / 2))
         lineLayer?.frame = CGRect(origin: origin, size: size)
         focusLayer?.frame = CGRect(x: origin.x, y: selectionLayerPosY, width: size.width, height: selectionLayerHeight)
     }
