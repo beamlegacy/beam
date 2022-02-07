8b98dde011e95391e8dc8233c819fdd9c375cc64
Remi Santos <remi@beamapp.co>
2022-02-07 15:45:40 +0000

    BE-3092 Improve onboarding import loading view and skip import if needed

--- Beam/Classes/Components/Onboarding/OnboardingEmailConfirmationView.swift
+++ Beam/Classes/Components/Onboarding/OnboardingEmailConfirmationView.swift
@@ -62,7 +62,7 @@ struct OnboardingEmailConfirmationView: View {
     var body: some View {
         VStack(spacing: 0) {
             if loadingState == .gettingInfos {
-                OnboardingView.LoadingView(randomDetails: ["account", "username"])
+                OnboardingView.LoadingView()
                     .transition(.opacity.animation(BeamAnimation.easeInOut(duration: 0.2)))
             } else {
                 VStack(spacing: 0) {
