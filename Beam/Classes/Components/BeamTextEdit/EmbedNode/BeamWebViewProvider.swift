9f414c94c76244acfa1b0ff59aa205de084260f7
Grégory de Jonckheere <greg@beamapp.co>
2022-02-24 05:31:31 +0000

    Fix embed display in stateless editors BE-675

--- Beam/Classes/Components/BeamTextEdit/EmbedNode/BeamWebViewProvider.swift
+++ Beam/Classes/Components/BeamTextEdit/EmbedNode/BeamWebViewProvider.swift
@@ -2,19 +2,21 @@ import Foundation
 import BeamCore
 
 /// Provides a web view to be used to display embeds.
-/// May reuse a web view currently playing in the media player manager.
+/// May reuse a web view currently playing in the editor's media player manager.
 final class BeamWebViewProvider {
 
-    private let note: BeamNote
+    private weak var editor: BeamTextEdit?
     private let elementId: UUID
     private let url: URL
-    private weak var mediaPlayerManager: NoteMediaPlayerManager?
 
-    init(note: BeamNote, elementId: UUID, url: URL, noteMediaPlayerManager: NoteMediaPlayerManager) {
-        self.note = note
+    private var mediaPlayerManager: NoteMediaPlayerManager? {
+        editor?.state?.noteMediaPlayerManager
+    }
+
+    init(editor: BeamTextEdit, elementId: UUID, url: URL) {
+        self.editor = editor
         self.elementId = elementId
         self.url = url
-        self.mediaPlayerManager = noteMediaPlayerManager
     }
 
 }
@@ -24,7 +26,8 @@ extension BeamWebViewProvider: BeamWebViewProviding {
     /// Provides a web view that may have been reused.
     /// - Parameter completion: A block executed with a web view and a boolean set to true if it was reused.
     func webView(_ completionHandler: @escaping (BeamWebView, Bool) -> Void) {
-        if let webView = mediaPlayerManager?.playingWebViewForNote(note: note, elementId: elementId, url: url) {
+        if let note = editor?.note as? BeamNote,
+           let webView = mediaPlayerManager?.playingWebViewForNote(note: note, elementId: elementId, url: url) {
             completionHandler(webView, true)
             return
         }
@@ -34,6 +37,7 @@ extension BeamWebViewProvider: BeamWebViewProviding {
             MediaPlayerMessageHandler(),
             EmbedNodeMessageHandler()
         ])
+
         let webView = BeamWebView(frame: .zero, configuration: configuration)
         webView.setupForEmbed()
         AppDelegate.main.data.setup(webView: webView)
