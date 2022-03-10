216e4481dbea371a35a5c6bbea29ef0d77a78ed0
quentin valero <quentin@beamapp.co>
2022-03-10 16:53:26 +0000

    [CI] adapt ref branch to cancel pipelines

--- scripts/delete-pipeline.sh
+++ scripts/delete-pipeline.sh
@@ -12,10 +12,21 @@ then
 	# Query running pipelines for the same branch, 
 	# process + filter them,
 	# and finally cancel them with another API call
+
+	# Need to handle Merge and Head refs
+	CI_MERGE_REQUEST_REF_PATH_MERGE=${CI_MERGE_REQUEST_REF_PATH/head/merge}
+	echo "CI_MERGE_REQUEST_REF_PATH_MERGE = ${CI_MERGE_REQUEST_REF_PATH_MERGE}"
+	curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/pipelines?ref=${CI_MERGE_REQUEST_REF_PATH_MERGE}&status=running" | \
+	jq '.[] | .id' | \
+	tail -n +2 | \
+	xargs -n 1 -I {} \
+	curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" -X POST "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/pipelines/{}/cancel"
+
 	echo "CI_MERGE_REQUEST_REF_PATH = ${CI_MERGE_REQUEST_REF_PATH}"
 	curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/pipelines?ref=${CI_MERGE_REQUEST_REF_PATH}&status=running" | \
 	jq '.[] | .id' | \
 	tail -n +2 | \
 	xargs -n 1 -I {} \
 	curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" -X POST "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/pipelines/{}/cancel"
+
 fi
