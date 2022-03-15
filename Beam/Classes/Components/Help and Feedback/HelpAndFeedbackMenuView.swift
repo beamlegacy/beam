daee42cc0c8f7a7750e6c2bef7e9fd2f1ae8e1ce
Grégory de Jonckheere <greg@beamapp.co>
2022-03-15 12:37:10 +0000

    Add help section subtitles and icons BE-2968

--- Beam/Classes/Components/Help and Feedback/HelpAndFeedbackMenuView.swift
+++ Beam/Classes/Components/Help and Feedback/HelpAndFeedbackMenuView.swift
@@ -30,6 +30,32 @@ enum HelpMenuSection: String, CaseIterable, Identifiable {
         }
     }
 
+    var subtitle: String? {
+        switch self {
+        case .shortcuts:
+            return "Beam like a pro"
+        case .featureRequest:
+            return "Submit an idea"
+        case .bugReport:
+            return "Found a bug? Help us squash it"
+        case .twitter:
+            return nil
+        }
+    }
+
+    var iconName: String {
+        switch self {
+        case .shortcuts:
+            return "help-shortcuts"
+        case .featureRequest:
+            return "help-featurerequest"
+        case .bugReport:
+            return "help-bug"
+        case .twitter:
+            return "help-twitter"
+        }
+    }
+
     var url: URL? {
         switch self {
         case .shortcuts:
@@ -62,27 +88,41 @@ struct HelpAndFeedbackMenuView: View {
                 ButtonLabel(icon: "tool-close", customStyle: ButtonLabelStyle(horizontalPadding: -2, activeBackgroundColor: Color.clear)) {
                     window?.close()
                 }
-            }.padding(.horizontal, 12)
+            }
+            .padding(.horizontal, 12)
             .frame(height: 41)
+
             PopupSeparator()
                 .padding(.vertical, 0)
-                .padding(.horizontal, 12)
+
             ForEach(HelpMenuSection.allCases) { section in
                 VStack(spacing: 0) {
-                    HStack {
-                        Text(section.text)
-                            .foregroundColor(BeamColor.Niobium.swiftUI)
-                            .font(BeamFont.regular(size: 12).swiftUI)
-                        Spacer()
-                    }.padding(.horizontal, 24)
-                    .padding(.top, 10)
-                    Spacer()
-                    if let last = HelpMenuSection.allCases.last, last != section {
-                        PopupSeparator()
-                            .padding(.vertical, 0)
-                            .padding(.horizontal, 12)
+                    VStack(alignment: .leading, spacing: 3) {
+                        HStack(spacing: 0) {
+                            Icon(name: section.iconName, width: 16, color: BeamColor.Niobium.swiftUI)
+                                .padding(.leading, 6)
+                                .padding(.trailing, 8)
+
+                            Text(section.text)
+                                .foregroundColor(BeamColor.Niobium.swiftUI)
+                                .font(BeamFont.medium(size: 12).swiftUI)
+
+                            Spacer()
+                        }
+
+                        if let subtitle = section.subtitle {
+                            Text(subtitle)
+                                .foregroundColor(BeamColor.LightStoneGray.swiftUI)
+                                .font(BeamFont.regular(size: 11).swiftUI)
+                                .padding(.leading, 30)
+                        }
                     }
-                }.frame(height: 39)
+                }
+                .padding(.leading, 10)
+                .padding(.trailing, 12)
+                .padding(.top, (section.subtitle != nil) ? 10 : 12)
+                .padding(.bottom, (section.subtitle != nil) ? 11 : 12)
+                .background(hoveredSection == section ? BeamColor.Nero.swiftUI : backgroundColor)
                 .onHover(perform: { hovering in
                     if hovering {
                         hoveredSection = section
@@ -90,7 +130,6 @@ struct HelpAndFeedbackMenuView: View {
                         hoveredSection = nil
                     }
                 })
-                .background(hoveredSection == section ? BeamColor.Nero.swiftUI : backgroundColor)
                 .onTapGesture {
                     if let link = section.url {
                         state.mode = .web
@@ -100,8 +139,15 @@ struct HelpAndFeedbackMenuView: View {
                     }
                     window?.close()
                 }
+
+                if let last = HelpMenuSection.allCases.last, last != section {
+                    PopupSeparator()
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 0)
+                }
             }
-        }.frame(width: Self.menuWidth)
+        }
+        .frame(width: Self.menuWidth)
         .background(backgroundColor)
         .cornerRadius(10)
     }
