2f7f2c71be0c9b449984530d88167add81cdd2c2
Grégory de Jonckheere <greg@beamapp.co>
2022-03-29 10:42:31 +0000

    Add embed loading placeholders BE-3251

--- Beam/Classes/Components/BeamTextEdit/EmbedNode/EmbedLoadingView.swift
+++ Beam/Classes/Components/BeamTextEdit/EmbedNode/EmbedLoadingView.swift
@@ -2,6 +2,10 @@ import AppKit
 
 final class EmbedLoadingView: NSView {
 
+    private var imageView: EmbedLoadingImageView!
+    private var imageName: String?
+    private let imageMinSize: CGFloat = 32
+
     init() {
         super.init(frame: .zero)
         prepare()
@@ -16,33 +20,91 @@ final class EmbedLoadingView: NSView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func layout() {
+        super.layout()
+
+        updateImage()
+    }
+
     override func viewDidChangeEffectiveAppearance() {
+        updateImage()
         updateColors()
+    }
+
+    func showImage(for provider: EmbedProvider) {
+        setImage(name: ImageName.imageName(for: provider))
+    }
+
+    func showDefaultImage() {
+        setImage(name: ImageName.default)
+    }
+
+    func showError() {
+        setImage(name: ImageName.error)
     }
 
     private func prepare() {
         layer = CALayer()
 
-        let attributedString = NSAttributedString(string: "Loading...", attributes: [
-            .font: BeamFont.regular(size: 13).nsFont,
-            .foregroundColor: BeamColor.AlphaGray.nsColor
-        ])
+        imageView = EmbedLoadingImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
 
-        let text = NSTextField(labelWithAttributedString: attributedString)
-        text.translatesAutoresizingMaskIntoConstraints = false
+        let requiredConstraints = [
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
 
-        addSubview(text)
+            imageView.widthAnchor.constraint(greaterThanOrEqualToConstant: imageMinSize),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+            imageView.heightAnchor.constraint(lessThanOrEqualTo: heightAnchor, multiplier: 1, constant: -16)
+        ]
 
-        addConstraints([
-            text.centerXAnchor.constraint(equalTo: centerXAnchor),
-            text.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
+        let lowerPriorityContraints = [
+            imageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.14)
+        ]
 
+        lowerPriorityContraints.forEach { $0.priority = .defaultLow }
+
+        addConstraints(requiredConstraints + lowerPriorityContraints)
+
+        updateImage()
         updateColors()
+    }
+
+    private func setImage(name: String) {
+        imageName = name
+        updateImage()
+    }
+
+    private func updateImage() {
+        guard
+            let imageName = imageName,
+            let image = NSImage(named: imageName)
+        else {
+            return
+        }
+
+        imageView.setImage(image)
     }
 
     private func updateColors() {
         layer?.backgroundColor = BeamColor.Nero.cgColor
+    }
+
+    // MARK: - ImageName
+
+    private enum ImageName {
+
+        static let `default` = "embed-logo_generic"
+        static let error = "embed-logo_error"
+
+        static func imageName(for provider: EmbedProvider) -> String {
+            switch provider {
+            case .unknown: return Self.default
+            default: return "embed-logo_\(provider.rawValue.lowercased())"
+            }
+        }
+
     }
 
 }
---
+++ Beam/Classes/Services/EmbedProvider.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum EmbedProvider: String, Codable {
+    case deviantArt = "DeviantArt"
+    case figma = "Figma"
+    case flickr = "Flickr"
+    case instagram = "Instagram"
+    case sketchfab = "Sketchfab"
+    case slideshare = "Slideshare"
+    case soundCloud = "SoundCloud"
+    case spotify = "Spotify"
+    case ted = "Ted"
+    case twitch = "Twitch"
+    case twitter = "Twitter"
+    case vimeo = "Vimeo"
+    case youTube = "YouTube"
+    case unknown
+}
