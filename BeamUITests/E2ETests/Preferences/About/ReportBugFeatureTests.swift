e836c7da4367b620ef641fb445c12420ff5d4ab1
Andrii Vasyliev <andrii@beamapp.co>
2022-04-12 11:22:26 +0000

    Report Bug/Feature from About preferences UI tests

---
+++ BeamUITests/E2ETests/Preferences/About/ReportBugFeatureTests.swift
@@ -0,0 +1,59 @@
+//
+//  ReportBugFeatureTests.swift
+//  BeamUITests
+//
+//  Created by Andrii on 07/04/2022.
+//
+
+import Foundation
+import XCTest
+
+class ReportBugFeatureTests: BaseTest {
+    
+    var aboutPreferencesView: AboutPreferencesTestView?
+    var webView: WebTestView?
+    
+    @discardableResult
+    private func openAboutPreferences() -> AboutPreferencesTestView {
+        ShortcutsHelper().shortcutActionInvoke(action: .openPreferences)
+        PreferencesBaseView().navigateTo(preferenceView: .about)
+        return AboutPreferencesTestView()
+    }
+    
+    func testBugFeatureWebPagesOpening() {
+        
+        let expectedBugsUrl = "https://beamapp.canny.io/bugs"
+        let expectedFeaturesUrl = "https://beamapp.canny.io/feature-r"
+        
+        step ("GIVEN I open About preferences") {
+            launchApp()
+            aboutPreferencesView = self.openAboutPreferences()
+        }
+        
+        step ("WHEN I click Report Feature button") {
+            aboutPreferencesView?.clickFeatureRequestButton()
+            ShortcutsHelper().shortcutActionInvoke(action: .close)
+            webView = WebTestView()
+        }
+        
+        step ("THEN I see \(expectedFeaturesUrl) URL opened") {
+            XCTAssertTrue(webView!
+                            .activateSearchFieldFromTab(index: 0)
+                            .waitForSearchFieldValueToEqual(expectedValue: expectedFeaturesUrl))
+        }
+        
+        step ("WHEN I click Report Bug button") {
+            self.openAboutPreferences()
+            aboutPreferencesView?.clickReportBugButton()
+            ShortcutsHelper().shortcutActionInvoke(action: .close)
+        }
+        
+        step ("THEN I see \(expectedBugsUrl) URL opened") {
+            XCTAssertTrue(webView!
+                            .activateSearchFieldFromTab(index: 1)
+                            .waitForSearchFieldValueToEqual(expectedValue: expectedBugsUrl))
+        }
+        
+    }
+    
+}
---
+++ BeamUITests/Sources/Locators/Preferences/AboutPreferencesViewLocators.swift
@@ -0,0 +1,17 @@
+//
+//  AboutPreferencesViewLocators.swift
+//  BeamUITests
+//
+//  Created by Andrii on 07/04/2022.
+//
+
+import Foundation
+
+enum AboutPreferencesViewLocators {
+    
+    enum Buttons: String, CaseIterable, UIElement {
+        case reportBugButton = "Report a bug..."
+        case reportFeatureButton = "Feature Request..."
+    }
+    
+}
