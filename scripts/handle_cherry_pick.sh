4fc667d8c3d6d51d14cbe21bef1f74d88be578c7
quentin valero <quentin@beamapp.co>
2022-04-15 07:11:46 +0000

    BE-3861 - add comment to original MR to find the cherry pick MR

--- scripts/handle_cherry_pick.sh
+++ scripts/handle_cherry_pick.sh
@@ -16,13 +16,19 @@ if [ "$LAST_DEVELOP_COMMIT" != "$RELEASE_COMMIT" ]; then
     # create branch for the cherry pick
     curl -sS -X POST -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/repository/branches?ref=$CI_COMMIT_SHA&branch=cherry-pick/$CI_COMMIT_SHORT_SHA"
 
-    # Get tile of cherry picked MR
+    # Get title and id of cherry picked MR
     MR_TITLE=$(curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA/merge_requests" | \
         jq '.[] | .title' | jq -sRr @uri)
+
+    ORIGINAL_MR_IID=$(curl -sS -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA/merge_requests" | \
+        jq '.[] | .iid')
 
     # create MR and get the iid
     MR_IID=$(curl -sS -X POST -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/merge_requests?squash=true&source_branch=cherry-pick/$CI_COMMIT_SHORT_SHA&target_branch=develop&remove_source_branch=true&title=Cherry%20Pick%20$MR_TITLE%20into%20develop&approvals_before_merge=0" | \
     	jq '.iid')
+
+    # Add note to original MR
+    curl -sS -X POST -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/merge_requests/$ORIGINAL_MR_IID/notes?body=MR%20cherry%20picked%20%21$MR_IID"
 
     # Auto Merge MR when it is possible to set up the flag
     until $(curl -sS --fail --output /dev/null --silent -X PUT -H "PRIVATE-TOKEN: $GITLAB_API_ACCESS_TOKEN" "${GITLAB_API_URL}/projects/$CI_PROJECT_ID/merge_requests/${MR_IID}/merge?merge_when_pipeline_succeeds=true"); do
