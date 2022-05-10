17c2f34773728b9d6a734fd2fb1145b054b0c0f3
Jerome Blondon <jbl@beamapp.co>
2022-05-10 14:34:07 +0000

    [BE-4046] AccountManager & UserInfoRequest now use async network code

--- Beam/Classes/Managers/AccountManager+Foundation.swift
+++ Beam/Classes/Managers/AccountManager+Foundation.swift
@@ -4,242 +4,185 @@ import Foundation
 import BeamCore
 
 extension AccountManager {
-    @discardableResult
-    func refreshToken(_ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
+    func refreshToken(_ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) {
         guard let accessToken = Persistence.Authentication.accessToken,
               let refreshToken = Persistence.Authentication.refreshToken else {
             completionHandler?(.success(false))
-            return nil
+            return
         }
 
-        do {
-            return try self.userSessionRequest.refreshToken(accessToken: accessToken, refreshToken: refreshToken) { result in
-                switch result {
-                case .failure(let error):
+        Task {
+            do {
+                let refresh = try await self.userSessionRequest.refreshToken(accessToken: accessToken, refreshToken: refreshToken)
+                guard let newAccessToken = refresh.accessToken, refresh.refreshToken != nil else {
                     Self.moveToSignedOff()
-                    completionHandler?(.failure(error))
-                case .success(let refresh):
-                    guard let newAccessToken = refresh.accessToken, refresh.refreshToken != nil else {
-                        Self.moveToSignedOff()
-                        completionHandler?(.failure(APIRequestError.parserError))
-                        return
-                    }
-
-                    Logger.shared.logInfo("Expiration \(String(describing: AuthenticationManager.expirationDate(accessToken))) -> \(String(describing: AuthenticationManager.expirationDate(newAccessToken)))", category: .accountManager)
-                    EventsTracker.logBreadcrumb(message: "Refreshed access token and refresh token",
-                                                       category: "app.lifecycle",
-                                                       type: "system")
-
-                    Persistence.Authentication.accessToken = refresh.accessToken
-                    Persistence.Authentication.refreshToken = refresh.refreshToken
-                    AuthenticationManager.shared.persistenceDidUpdate()
-                    Self.moveToAuthenticated()
-                    completionHandler?(.success(true))
+                    completionHandler?(.failure(APIRequestError.parserError))
+                    return
                 }
-            }
-        } catch {
-            Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
-            Self.moveToSignedOff()
-            completionHandler?(.failure(error))
-        }
 
-        return nil
+                Logger.shared.logInfo("Expiration \(String(describing: AuthenticationManager.expirationDate(accessToken))) -> \(String(describing: AuthenticationManager.expirationDate(newAccessToken)))", category: .accountManager)
+                EventsTracker.logBreadcrumb(message: "Refreshed access token and refresh token",
+                                            category: "app.lifecycle",
+                                            type: "system")
+
+                Persistence.Authentication.accessToken = refresh.accessToken
+                Persistence.Authentication.refreshToken = refresh.refreshToken
+                AuthenticationManager.shared.persistenceDidUpdate()
+                Self.moveToAuthenticated()
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
+                Self.moveToSignedOff()
+                completionHandler?(.failure(error))
+            }
+        }
     }
 
-    @discardableResult
     func signIn(email: String,
                 password: String,
                 runFirstSync: Bool,
                 completionHandler: ((Result<Bool, Error>) -> Void)? = nil,
-                syncCompletion: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userSessionRequest.signIn(email: email, password: password) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
-                    Self.moveToSignedOff()
-                    completionHandler?(.failure(error))
-                case .success(let signIn):
-                    Persistence.Authentication.accessToken = signIn.accessToken
-                    Persistence.Authentication.refreshToken = signIn.refreshToken
-                    Persistence.Authentication.email = email
-                    Persistence.Authentication.password = password
-                    AuthenticationManager.shared.persistenceDidUpdate()
-                    ThirdPartyLibrariesManager.shared.updateUser()
+                syncCompletion: ((Result<Bool, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                let signIn = try await userSessionRequest.signIn(email: email, password: password)
+                Persistence.Authentication.accessToken = signIn.accessToken
+                Persistence.Authentication.refreshToken = signIn.refreshToken
+                Persistence.Authentication.email = email
+                Persistence.Authentication.password = password
+                AuthenticationManager.shared.persistenceDidUpdate()
+                ThirdPartyLibrariesManager.shared.updateUser()
 
-                    EncryptionManager.shared.privateKey(for: email)
+                EncryptionManager.shared.privateKey(for: email)
 
-                    // Syncing with remote API, AppDelegate needs to be called in mainthread
-                    // TODO: move this syncData to a manager instead.
-                    if runFirstSync {
-                        DispatchQueue.main.async {
-                            self.runFirstSync(useBuiltinPrivateKeyUI: true, syncCompletion: syncCompletion)
-                        }
+                // Syncing with remote API, AppDelegate needs to be called in mainthread
+                // TODO: move this syncData to a manager instead.
+                if runFirstSync {
+                    DispatchQueue.main.async {
+                        self.runFirstSync(useBuiltinPrivateKeyUI: true, syncCompletion: syncCompletion)
                     }
-
-                    Self.moveToAuthenticated()
-                    completionHandler?(.success(true))
                 }
+
+                Self.moveToAuthenticated()
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
+                Self.moveToSignedOff()
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
-            Self.moveToSignedOff()
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
     func signInWithProvider(provider: IdentityRequest.Provider,
                             accessToken: String,
                             runFirstSync: Bool,
                             completionHandler: ((Result<Bool, Error>) -> Void)? = nil,
-                            syncCompletion: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userSessionRequest.signInWithProvider(provider: provider, accessToken: accessToken) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
-                    Self.moveToSignedOff()
-                    completionHandler?(.failure(error))
-                case .success(let signIn):
-                    Persistence.Authentication.accessToken = signIn.accessToken
-                    Persistence.Authentication.refreshToken = signIn.refreshToken
-                    if Persistence.Authentication.email != signIn.me?.email {
-                        Persistence.Authentication.email = signIn.me?.email
-                        Persistence.Authentication.password = nil
-                    }
-                    EncryptionManager.shared.privateKey(for: Persistence.emailOrRaiseError())
-                    AuthenticationManager.shared.persistenceDidUpdate()
-                    ThirdPartyLibrariesManager.shared.updateUser()
+                            syncCompletion: ((Result<Bool, Error>) -> Void)? = nil) {
 
-                    // Syncing with remote API, AppDelegate needs to be called in mainthread
-                    // TODO: move this syncData to a manager instead.
-                    if runFirstSync {
-                        DispatchQueue.main.async {
-                            self.runFirstSync(useBuiltinPrivateKeyUI: true, syncCompletion: syncCompletion)
-                        }
-                    }
-                    Self.moveToAuthenticated()
-                    completionHandler?(.success(true))
+        Task {
+            do {
+                let signIn = try await userSessionRequest.signInWithProvider(provider: provider, accessToken: accessToken)
+                Persistence.Authentication.accessToken = signIn.accessToken
+                Persistence.Authentication.refreshToken = signIn.refreshToken
+                if Persistence.Authentication.email != signIn.me?.email {
+                    Persistence.Authentication.email = signIn.me?.email
+                    Persistence.Authentication.password = nil
                 }
+                EncryptionManager.shared.privateKey(for: Persistence.emailOrRaiseError())
+                AuthenticationManager.shared.persistenceDidUpdate()
+                ThirdPartyLibrariesManager.shared.updateUser()
+
+                // Syncing with remote API, AppDelegate needs to be called in mainthread
+                // TODO: move this syncData to a manager instead.
+                if runFirstSync {
+                    DispatchQueue.main.async {
+                        self.runFirstSync(useBuiltinPrivateKeyUI: true, syncCompletion: syncCompletion)
+                    }
+                }
+                Self.moveToAuthenticated()
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
+                Self.moveToSignedOff()
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not signin: \(error.localizedDescription)", category: .accountManager)
-            Self.moveToSignedOff()
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
     func signUp(_ email: String,
                 _ password: String,
-                _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userSessionRequest.signUp(email, password) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not sign up: \(error.localizedDescription)", category: .accountManager)
-                    completionHandler?(.failure(error))
-                case .success(let signUp):
-                    Logger.shared.logInfo("signUp succeeded: \(signUp.user?.email ?? "-")", category: .accountManager)
-                    completionHandler?(.success(true))
-                }
+                _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                let signUp = try await userSessionRequest.signUp(email, password)
+                Logger.shared.logInfo("signUp succeeded: \(signUp.user?.email ?? "-")", category: .accountManager)
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not sign up: \(error.localizedDescription)", category: .accountManager)
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not sign up: \(error.localizedDescription)", category: .accountManager)
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
     func forgotPassword(email: String,
-                        _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userSessionRequest.forgotPassword(email: email) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not forgot password: \(error.localizedDescription)", category: .accountManager)
-                    completionHandler?(.failure(error))
-                case .success:
-                    Logger.shared.logInfo("forgot Password succeeded", category: .accountManager)
-                    completionHandler?(.success(true))
-                }
+                        _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                try await userSessionRequest.forgotPassword(email: email)
+                Logger.shared.logInfo("forgot Password succeeded", category: .accountManager)
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not forgot password: \(error.localizedDescription)", category: .accountManager)
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not forgot password: \(error.localizedDescription)", category: .accountManager)
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
     func resendVerificationEmail(email: String,
-                                 _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userSessionRequest.resendVerificationEmail(email: email) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not resend verification email: \(error.localizedDescription)", category: .accountManager)
-                    completionHandler?(.failure(error))
-                case .success:
-                    Logger.shared.logInfo("resend verification email succeeded", category: .accountManager)
-                    completionHandler?(.success(true))
-                }
+                                 _ completionHandler: ((Result<Bool, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                try await userSessionRequest.resendVerificationEmail(email: email)
+                Logger.shared.logInfo("resend verification email succeeded", category: .accountManager)
+                completionHandler?(.success(true))
+            } catch {
+                Logger.shared.logInfo("Could not resend verification email: \(error.localizedDescription)", category: .accountManager)
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not resend verification email: \(error.localizedDescription)", category: .accountManager)
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
-    func getUserInfos(_ completionHandler: ((Result<UserInfoRequest.UserInfos, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userInfoRequest.getUserInfos { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not get user infos: \(error.localizedDescription)", category: .accountManager)
-                    completionHandler?(.failure(error))
-                case .success(let infos):
-                    Logger.shared.logInfo("Get user infos succeeded", category: .accountManager)
-                    AuthenticationManager.shared.username = infos.username
-                    completionHandler?(.success(infos))
-                }
+    func getUserInfos(_ completionHandler: ((Result<UserInfoRequest.UserInfos, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                let infos = try await userInfoRequest.getUserInfos()
+                Logger.shared.logInfo("Get user infos succeeded", category: .accountManager)
+                AuthenticationManager.shared.username = infos.username
+                completionHandler?(.success(infos))
+            } catch {
+                Logger.shared.logInfo("Could not get user infos: \(error.localizedDescription)", category: .accountManager)
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not get user infos: \(error.localizedDescription)", category: .accountManager)
-            completionHandler?(.failure(error))
         }
-        return nil
     }
 
-    @discardableResult
-    func setUsername(username: String, _ completionHandler: ((Result<String, Error>) -> Void)? = nil) -> URLSessionTask? {
-        do {
-            return try userInfoRequest.setUsername(username: username) { result in
-                switch result {
-                case .failure(let error):
-                    Logger.shared.logInfo("Could not set username: \(error.localizedDescription)", category: .accountManager)
-                    completionHandler?(.failure(error))
-                case .success(let infos):
-                    guard let username = infos.me?.username else {
-                        completionHandler?(.failure(APIRequestError.parserError))
-                        return
-                    }
-                    Logger.shared.logInfo("Set username succeeded", category: .accountManager)
-                    AuthenticationManager.shared.username = username
-                    completionHandler?(.success(username))
+    func setUsername(username: String, _ completionHandler: ((Result<String, Error>) -> Void)? = nil) {
+        Task {
+            do {
+                let infos = try await userInfoRequest.setUsername(username: username)
+                guard let username = infos.me?.username else {
+                    completionHandler?(.failure(APIRequestError.parserError))
+                    return
                 }
+                Logger.shared.logInfo("Set username succeeded", category: .accountManager)
+                AuthenticationManager.shared.username = username
+                completionHandler?(.success(username))
+            } catch {
+                Logger.shared.logInfo("Could not set username: \(error.localizedDescription)", category: .accountManager)
+                completionHandler?(.failure(error))
             }
-        } catch {
-            Logger.shared.logInfo("Could not set username: \(error.localizedDescription)", category: .accountManager)
-            completionHandler?(.failure(error))
+
         }
-        return nil
     }
 
     static func logout() {
