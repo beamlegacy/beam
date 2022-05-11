b5467609d61b5a6dd3884d446261ee55200aa200
Remi Santos <remi@beamapp.co>
2022-05-11 15:20:43 +0000

    BE-2291 Allow drag browsertab out of window

---
+++ Beam/Classes/Views/Toolbar/Tabs/TabsExternalDraggingSource.swift
@@ -0,0 +1,165 @@
+//
+//  TabDragSession.swift
+//  Beam
+//
+//  Created by Remi Santos on 19/04/2022.
+//
+
+import Foundation
+import Combine
+import AppKit
+import UniformTypeIdentifiers
+import SwiftUI
+
+extension UTType {
+    static let beamBrowserTab = UTType("co.beamapp.browsertab") ?? .data
+}
+
+extension NSPasteboard.PasteboardType {
+    static let beamBrowserTab = NSPasteboard.PasteboardType(UTType.beamBrowserTab.description)
+}
+
+class BrowserTabPasteboardProvider: NSObject, NSPasteboardItemDataProvider {
+    private var url: URL?
+    private weak var browserTab: BrowserTab?
+
+    init(with tab: BrowserTab) {
+        self.url = tab.url
+        self.browserTab = tab
+    }
+
+    func pasteboard(_ pasteboard: NSPasteboard?, item: NSPasteboardItem, provideDataForType type: NSPasteboard.PasteboardType) {
+        if type == .URL, let url = url {
+            pasteboard?.setString(url.absoluteString, forType: .URL)
+        } else if type == .beamBrowserTab, let tab = browserTab {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(tab) {
+                pasteboard?.setData(data, forType: .beamBrowserTab)
+            }
+        }
+    }
+
+    func pasteboardFinishedWithDataProvider(_ pasteboard: NSPasteboard) { }
+}
+
+/// Simple Tab View used to render the dragging session image
+private struct TabViewForSnapshot: View {
+    var tab: BrowserTab
+    var size: CGSize
+    var body: some View {
+        TabView(tab: tab,
+                isHovering: false,
+                isSelected: true,
+                isPinned: false,
+                isSingleTab: false,
+                isDragging: true,
+                disableHovering: true,
+                applyDraggingStyle: false)
+            .frame(width: size.width, height: size.height)
+        .drawingGroup() // fixes rendering issue with images when snapshoting.
+    }
+}
+
+protocol TabExternalDraggingSourceDelegate: AnyObject {
+    func tabExternalDragSessionEnded()
+}
+
+class TabExternalDraggingSource: NSObject {
+
+    private weak var state: BeamState?
+    private weak var delegate: TabExternalDraggingSourceDelegate?
+    private var browserTab: BrowserTab?
+    private var cancellables = Set<AnyCancellable>()
+
+    private var isDropHandledByBeamUI = false
+    private var draggingImage: NSImage?
+    private var transparentDraggingImage: NSImage?
+
+    init(state: BeamState, delegate: TabExternalDraggingSourceDelegate) {
+        self.state = state
+        self.delegate = delegate
+        super.init()
+        state.data.$currentDraggingSession.sink { [weak self] dataSession in
+            guard let dataSession = dataSession, let draggingSession = dataSession.draggingSession else { return }
+            self?.willBeHandledByBeamUI(dataSession.dropHandledByBeamUI, session: draggingSession)
+        }.store(in: &cancellables)
+    }
+
+    private func draggingItemFrame(location: CGPoint, convertToWindow window: NSWindow?) -> CGRect {
+        var point = location
+        if let window = window {
+            point = location.flippedPointToBottomLeftOrigin(in: window)
+        }
+
+        let size = CGSize(width: 220, height: TabView.height)
+        return CGRect(x: point.x, y: point.y,
+                      width: size.width, height: size.height)
+    }
+
+    func startDraggingItem(for tab: BrowserTab, location: CGPoint) -> NSDraggingItem {
+        let pasteboardItem = NSPasteboardItem()
+        pasteboardItem.setDataProvider(BrowserTabPasteboardProvider(with: tab), forTypes: [ .beamBrowserTab])
+
+        let dragItem = NSDraggingItem(pasteboardWriter: pasteboardItem)
+        let itemFrame = draggingItemFrame(location: location, convertToWindow: state?.associatedWindow)
+
+        let view = TabViewForSnapshot(tab: tab, size: itemFrame.size)
+        self.draggingImage = view.snapshot()
+        // using transparent image to avoid automatic sizing animation when setting a nil image
+        self.transparentDraggingImage = NSImage(size: itemFrame.size, flipped: false, drawingHandler: { _ in true })
+        dragItem.setDraggingFrame(itemFrame, contents: self.draggingImage)
+
+        self.browserTab = tab
+        self.isDropHandledByBeamUI = false
+        return dragItem
+    }
+
+    func updateInitialDragginItemLocation(_ dragItem: NSDraggingItem, location: CGPoint) {
+        let itemFrame = draggingItemFrame(location: location, convertToWindow: state?.associatedWindow)
+        dragItem.setDraggingFrame(itemFrame, contents: self.draggingImage)
+    }
+
+    private func willBeHandledByBeamUI(_ handled: Bool, session: NSDraggingSession) {
+        guard handled != self.isDropHandledByBeamUI else { return }
+        isDropHandledByBeamUI = handled
+        session.enumerateDraggingItems(options: .concurrent,
+                                       for: state?.associatedWindow?.contentView,
+                                       classes: [NSPasteboardItem.self]) { [weak self] item, _, _ in
+            item.setDraggingFrame(draggingItemFrame(location: session.draggingLocation, convertToWindow: nil),
+                                  contents: handled ? self?.transparentDraggingImage : self?.draggingImage)
+        }
+    }
+}
+
+extension TabExternalDraggingSource: NSDraggingSource {
+
+    func draggingSession(_ session: NSDraggingSession, sourceOperationMaskFor context: NSDraggingContext) -> NSDragOperation {
+        .move
+    }
+
+    func draggingSession(_ session: NSDraggingSession, willBeginAt screenPoint: NSPoint) { }
+
+    func draggingSession(_ session: NSDraggingSession, movedTo screenPoint: NSPoint) { }
+
+    func draggingSession(_ session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
+        state?.data.currentDraggingSession = nil
+        delegate?.tabExternalDragSessionEnded()
+        guard let tab = self.browserTab, !self.isDropHandledByBeamUI else { return }
+
+        state?.browserTabsManager.removeTab(tabId: tab.id, suggestedNextCurrentTab: nil)
+
+        if state?.browserTabsManager.tabs.isEmpty == true, let window = state?.associatedWindow as? BeamWindow {
+            // tab was dragged out from a window with only 1 tab. bring back the originated window that was hidden.
+            let frameOrigin = CGPoint(x: max(0, screenPoint.x - (window.frame.width / 2)),
+                                      y: max(0, screenPoint.y - window.frame.height + (Toolbar.height / 2)))
+            window.setFrameOrigin(frameOrigin)
+
+            window.state.browserTabsManager.addNewTabAndGroup(tab, setCurrent: true)
+            window.state.mode = .web
+            window.makeKeyAndOrderFront(nil)
+        } else {
+            AppDelegate.main.createWindow(withTabs: [tab], at: screenPoint)
+        }
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
---
+++ Beam/Classes/Views/Toolbar/Tabs/TabsListExternalDragViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  TabsListExternalDragViewModel.swift
+//  Beam
+//
+//  Created by Remi Santos on 30/04/2022.
+//
+
+import Foundation
+import BeamCore
+
+class TabsListExternalDragViewModel: ObservableObject {
+
+    @Published var isDraggingTabOutside = false
+    var dropStartLocation: CGPoint?
+    var isDroppingAnExternalTab = false
+
+    private weak var data: BeamData?
+    private weak var state: BeamState?
+    private weak var tabsManager: BrowserTabsManager?
+
+    func setup(withState state: BeamState?, tabsMananger: BrowserTabsManager?) {
+        self.data = state?.data
+        self.state = state
+        self.tabsManager = tabsMananger
+    }
+
+    /// Prepare the dragging source and item image, but doesn't start the dragging yet.
+    func prepareExternalDraggingOfcurrentTab(atLocation location: CGPoint) {
+        guard data?.currentDraggingSession == nil, let state = self.state, let tab = tabsManager?.currentTab else { return }
+        let dragSource = TabExternalDraggingSource(state: state, delegate: self)
+        let dragItem = dragSource.startDraggingItem(for: tab, location: location)
+        data?.currentDraggingSession = .init(draggedObject: tab, draggingSource: dragSource, draggingItem: dragItem)
+    }
+
+    /// Triggers the actual dragging session with the previously prepared dragging source and item
+    func startExternalDraggingOfCurrentTab(atLocation location: CGPoint) {
+        guard !isDraggingTabOutside else { return }
+        isDraggingTabOutside = true
+
+        guard data?.currentDraggingSession?.draggingSession == nil, let event = NSApp.currentEvent,
+              let currentTab = tabsManager?.currentTab, let view = state?.associatedWindow?.contentView else { return }
+
+        tabsManager?.removeTab(tabId: currentTab.id, suggestedNextCurrentTab: nil)
+        if tabsManager?.tabs.isEmpty == true {
+            // hide the origin window, we will close it if tab is moved to another window, or unhide instead of creating a new one.
+            state?.associatedWindow?.orderOut(self)
+        }
+
+        let performer = NSHapticFeedbackManager.defaultPerformer
+        performer.perform(.alignment, performanceTime: .default)
+
+        if let dragSource = data?.currentDraggingSession?.draggingSource as? TabExternalDraggingSource,
+            let dragItem = data?.currentDraggingSession?.draggingItem {
+            dragSource.updateInitialDragginItemLocation(dragItem, location: location)
+        } else {
+            prepareExternalDraggingOfcurrentTab(atLocation: location)
+        }
+
+        guard let dragSource = data?.currentDraggingSession?.draggingSource,
+              let dragItem = data?.currentDraggingSession?.draggingItem else {
+                  Logger.shared.logError("Couldn't start tab external dragging session", category: .ui)
+                  return
+              }
+
+        DispatchQueue.main.async { // starting the drag async to let the internal drag gesture finish properly
+            let dragSession = view.beginDraggingSession(with: [dragItem], event: event, source: dragSource)
+            dragSession.animatesToStartingPositionsOnCancelOrFail = false
+            self.data?.currentDraggingSession?.draggingSession = dragSession
+        }
+    }
+
+    func dropOfExternalTabStarted(atLocation location: CGPoint) {
+        data?.currentDraggingSession?.dropHandledByBeamUI = true
+        dropStartLocation = location
+        isDroppingAnExternalTab = true
+        isDraggingTabOutside = false
+    }
+
+    func dropOfExternalTabExitedWindow(tab: BrowserTab) {
+        data?.currentDraggingSession?.dropHandledByBeamUI = false
+        tabsManager?.removeTab(tabId: tab.id, suggestedNextCurrentTab: nil)
+    }
+}
+
+extension TabsListExternalDragViewModel: TabExternalDraggingSourceDelegate {
+    func tabExternalDragSessionEnded() {
+        isDraggingTabOutside = false
+    }
+}
