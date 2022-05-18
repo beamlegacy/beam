ed9650eab10cc4461239999b360ed870bb13e0b6
Andrii Vasyliev <andrii@beamapp.co>
2022-05-18 14:21:36 +0000

    Preferences Credit card UI tests

---
+++ BeamUITests/Sources/Locators/Tables/CreditCardTableLocators.swift
@@ -0,0 +1,24 @@
+//
+//  CreditCardTableLocators.swift
+//  BeamUITests
+//
+//  Created by Andrii Vasyliev on 11.05.2022.
+//
+
+import Foundation
+
+enum CreditCardTableLocators {
+    
+    enum TextFields: String, CaseIterable, UIElement {
+        case descriptionTextField = "preferences credit card"
+        case cardHolderTextField = "Cardholder"
+        case cardNumberTextField = "Card Number"
+        case cardDateTextField = "Card Date"
+    }
+    
+    enum Buttons: String, CaseIterable, UIElement {
+        case cancelDeletionButton = "Cancel"
+        case confirmDeletionButton = "Remove"
+    }
+    
+}
