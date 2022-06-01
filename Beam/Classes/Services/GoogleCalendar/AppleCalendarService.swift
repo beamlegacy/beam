987cf8e2cc65a910fe367b93a5c6451083689cfd
Frank Lefebvre <frank@beamapp.co>
2022-06-01 10:13:25 +0000

    BE-3876 Calendar integration improvements

--- Beam/Classes/Services/GoogleCalendar/AppleCalendarService.swift
+++ Beam/Classes/Services/GoogleCalendar/AppleCalendarService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import BeamCore
 import EventKit
+import Contacts
 
 final class AppleCalendarService {
     let id = UUID()
@@ -16,6 +17,13 @@ final class AppleCalendarService {
     var inNeedOfPermission = true
 
     private let eventStore = EKEventStore()
+    private let contactStore = CNContactStore()
+
+    private func getMeetingsWithCalendarPermission(for dateMin: Date, and dateMax: Date?, onlyToday: Bool, query: String?, completionHandler: @escaping (Result<[Meeting]?, CalendarError>) -> Void) {
+        contactStore.requestAccess(for: .contacts) { _, _ in
+            self.getMeetingsWithPermission(for: dateMin, and: dateMax, onlyToday: onlyToday, query: query, completionHandler: completionHandler)
+        }
+    }
 
     private func getMeetingsWithPermission(for dateMin: Date, and dateMax: Date?, onlyToday: Bool, query: String?, completionHandler: @escaping (Result<[Meeting]?, CalendarError>) -> Void) {
         let endDate = onlyToday ? dateMin.isoCalEndOfDay : dateMax ?? .distantFuture // same behavior as GoogleCalendarService: dateMax is ignored if onlyToday is true
@@ -31,15 +39,48 @@ final class AppleCalendarService {
                 let startTime: Date = event.startDate
                 let endTime = event.endDate
                 let allDayEvent = event.isAllDay
-                let attendees = (event.attendees ?? []).map { (attendee: EKParticipant) -> Meeting.Attendee in
-                    Meeting.Attendee(email: "", name: attendee.name ?? "")
-                }
-                return Meeting(name: name, startTime: startTime, endTime: endTime, allDayEvent: allDayEvent, attendees: attendees, htmlLink: event.url?.absoluteString, meetingLink: nil)
+                let attendees = (event.attendees ?? [])
+                    .filter { $0.participantType == .person || $0.participantType == .group }
+                    .map { (attendee: EKParticipant) -> Meeting.Attendee in
+                        Meeting.Attendee(email: attendee.participantEmail ?? "", name: attendee.participantName(from: contactStore) ?? "")
+                    }
+                return Meeting(name: name, startTime: startTime, endTime: endTime, allDayEvent: allDayEvent, attendees: attendees, htmlLink: event.url?.absoluteString, meetingLink: event.meetingLink)
             }
             .sorted { meeting1, meeting2 in
                 meeting1.startTime < meeting2.startTime
             }
         completionHandler(.success(meetings))
+    }
+}
+
+private extension EKEvent {
+    var meetingLink: String? {
+        if let url = url {
+            return url.absoluteString
+        }
+        guard let link = notes?
+            .components(separatedBy: .newlines)
+            .first(where: { $0.hasPrefix("Join: ") })
+        else {
+            return nil
+        }
+        return link.dropFirst(6).trimmingCharacters(in: .whitespaces)
+    }
+}
+
+private extension EKParticipant {
+    func participantName(from contactStore: CNContactStore) -> String? {
+        do {
+            let contacts: [CNContact] = try contactStore.unifiedContacts(matching: contactPredicate, keysToFetch: [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)])
+            if let contact = contacts.first {
+                return CNContactFormatter().string(from: contact)
+            }
+        } catch {}
+        return name?.split(separator: "@").first.map(String.init)
+    }
+
+    var participantEmail: String? {
+        url.absoluteStringWithoutScheme?.trimmingCharacters(in: CharacterSet(charactersIn: ":"))
     }
 }
 
@@ -64,13 +105,13 @@ extension AppleCalendarService: CalendarService {
         if inNeedOfPermission {
             requestAccess { granted in
                 if granted {
-                    self.getMeetingsWithPermission(for: dateMin, and: dateMax, onlyToday: onlyToday, query: query, completionHandler: completionHandler)
+                    self.getMeetingsWithCalendarPermission(for: dateMin, and: dateMax, onlyToday: onlyToday, query: query, completionHandler: completionHandler)
                 } else {
                     completionHandler(.failure(CalendarError.permissionDenied))
                 }
             }
         } else {
-            getMeetingsWithPermission(for: dateMin, and: dateMax, onlyToday: onlyToday, query: query, completionHandler: completionHandler)
+            getMeetingsWithCalendarPermission(for: dateMin, and: dateMax, onlyToday: onlyToday, query: query, completionHandler: completionHandler)
         }
     }
 
