9b37af797d9d73d20473b336ed53e0df2c05ba6e
quentin valero <quentin@beamapp.co>
2022-06-06 11:49:23 +0000

    Auto test to hide cookie banner BE-4062

---
+++ BeamUITests/E2ETests/Privacy/Annoyances/CookieBannerBlockerTests.swift
@@ -0,0 +1,76 @@
+//
+//  CookieBannerBlockerTests.swift
+//  BeamUITests
+//
+//  Created by Quentin Valero on 03/06/2022.
+//
+
+import Foundation
+import XCTest
+
+class CookieBannerBlockerTests: BaseTest {
+    
+    let mockHttpPage = MockHTTPWebPages()
+    let uiMenu = UITestsMenuBar()
+    let shortcutHelper = ShortcutsHelper()
+    let privacyWindow = PrivacyPreferencesTestView()
+    let url = MockHTTPWebPages().getMockPageUrl(.cookieBannerAdBlock)
+    let app = XCUIApplication().windows["AdBlock"].webViews["AdBlock"]
+    let cookieBannerElements = [
+        "THIS WEBSITE USES COOKIES",
+        "We use cookies to personalise content and ads, to provide social media features and to analyse our traffic.",
+        "OK ",
+        "Decline"
+    ]
+
+    override func setUpWithError() throws {
+        launchApp()
+        uiMenu.destroyDB()
+            .startMockHTTPServer()
+        shortcutHelper.shortcutActionInvoke(action: .openPreferences)
+        PreferencesBaseView().navigateTo(preferenceView: .privacy)
+    }
+    
+    private func verifyCookieBannerDisplayed(_ status: Bool){
+        for cookieBannerElement in cookieBannerElements {
+            XCTAssertEqual(app.staticTexts[cookieBannerElement].exists, status)
+        }
+    }
+    
+    func testEnabledCookieBannerBlocker(){
+        
+        step ("Given Hide Cookie Banner blocker setting is enabled"){
+            if (!privacyWindow.isSettingEnabled(element: privacyWindow.getCookieBannerBlockerSettingElement())) {
+                privacyWindow.clickCookieBannerBlockerSetting()
+            }
+            XCTAssertTrue(privacyWindow.isSettingEnabled(element: privacyWindow.getCookieBannerBlockerSettingElement()))
+        }
+        
+        step ("When I navigate to \(url)"){
+            mockHttpPage.openMockPage(.cookieBannerAdBlock)
+        }
+        
+        step ("Then cookie banner is enabled displayed"){
+            verifyCookieBannerDisplayed(false)
+        }
+    }
+    
+    func testDisabledCookieBannerBlocker(){
+        
+        step ("Given Hide Cookie Banner button blocker setting is disabled"){
+            if (privacyWindow.isSettingEnabled(element: privacyWindow.getCookieBannerBlockerSettingElement())) {
+                privacyWindow.clickCookieBannerBlockerSetting()
+            }
+            XCTAssertFalse(privacyWindow.isSettingEnabled(element: privacyWindow.getCookieBannerBlockerSettingElement()))
+        }
+        
+        step ("When I navigate to \(url)"){
+            mockHttpPage.openMockPage(.cookieBannerAdBlock)
+        }
+        
+        step ("Then cookie banner is displayed"){
+            verifyCookieBannerDisplayed(true)
+        }
+    }
+    
+}
