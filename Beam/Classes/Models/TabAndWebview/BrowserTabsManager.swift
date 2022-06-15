72f04eec505b1f4996ea08c85bbc487beccbc544
Jean-Louis Darmon <luis@beamapp.co>
2022-06-15 08:40:09 +0000

    Experiments on TabColoring/Clustering stopping from working after a while - BE-4067

--- Beam/Classes/Models/TabAndWebview/BrowserTabsManager.swift
+++ Beam/Classes/Models/TabAndWebview/BrowserTabsManager.swift
@@ -392,6 +392,7 @@ extension BrowserTabsManager {
                 }
             }
             setCurrentTab(newCurrentTab)
+            data.clusteringManager.updateTabGroupsWithOpenPages()
         }
     }
 }
