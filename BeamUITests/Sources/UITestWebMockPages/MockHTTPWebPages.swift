5ab40e7e3c70a0f6fdf9b4d441199d5878938e93
Frank Lefebvre <frank@beamapp.co>
2022-07-01 18:28:45 +0000

    Change the mock server port to something uncommon BE-4465

--- BeamUITests/Sources/UITestWebMockPages/MockHTTPWebPages.swift
+++ BeamUITests/Sources/UITestWebMockPages/MockHTTPWebPages.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import XCTest
+import BeamCore
 
 class MockHTTPWebPages: BaseView {
     
@@ -62,77 +63,151 @@ class MockHTTPWebPages: BaseView {
         return getElementStringValue(element: element)
     }
     
-    enum MockPageLink: String, CaseIterable {
-        case mainView = "http://localhost:8080/"
-        case mockBaseUrl = "http://form.lvh.me:8080/"
-        
-        case ambiguousShortForm = "http://ambiguous.form.lvh.me:8080/"
-        case signupShortForm = "http://signup.form.lvh.me:8080/"
-        case signinShortForm = "http://signin.form.lvh.me:8080/"
-        case paymentNetflixShortForm = "http://payment-netflix.form.lvh.me:8080/"
-        
-        case signupForm = "http://form.lvh.me:8080/signup"
-        case signinForm = "http://form.lvh.me:8080/signin"
-        case signin1Form = "http://form.lvh.me:8080/signinpage1"
-        case signin2Form = "http://form.lvh.me:8080/signinpage2"
-        case signin3Form = "http://form.lvh.me:8080/signinpage3"
-        case signin4Form = "http://form.lvh.me:8080/signinpage4"
-        case signin5Form = "http://form.lvh.me:8080/signinpage5"
-        case signin61Form = "http://form.lvh.me:8080/signinpage6-1"
-        case signin71Form = "http://form.lvh.me:8080/signinpage7-1"
-        case signin8Form = "http://form.lvh.me:8080/signinpage8"
-        case signin91Form = "http://form.lvh.me:8080/signinpage9-1"
-        case signin92Form = "http://form.lvh.me:8080/signinpage9-2"
-        case signinstep2Form = "http://form.lvh.me:8080/signinstep2"
-        case signinebayForm = "http://form.lvh.me:8080/signinebay"
-        case signup1Form = "http://form.lvh.me:8080/signuppage1"
-        case signup2Form = "http://form.lvh.me:8080/signuppage2"
-        case signup3Form = "http://form.lvh.me:8080/signuppage3"
-        case signup4Form = "http://form.lvh.me:8080/signuppage4"
-        case signup51Form = "http://form.lvh.me:8080/signuppage5-1"
-        case signup6Form = "http://form.lvh.me:8080/signuppage6"
-        
-        case paymentForm = "http://form.lvh.me:8080/payment"
-        case paymentKiwiForm = "http://form.lvh.me:8080/payment-kiwi"
-        case paymentOplataForm = "http://form.lvh.me:8080/payment-oplata"
-        case paymentBookingForm = "http://form.lvh.me:8080/payment-booking"
-        case paymentSportDecouverteForm = "http://form.lvh.me:8080/payment-sport-decouverte"
-        case paymentAmazonForm = "http://form.lvh.me:8080/payment-amazon"
-        case paymentEbayForm = "http://form.lvh.me:8080/payment-ebay"
-        case paymentWalmartForm = "http://form.lvh.me:8080/payment-walmart"
-        case paymentEtsySearsForm = "http://form.lvh.me:8080/payment-etsy-sears"
-        case paymentWishForm = "http://form.lvh.me:8080/payment-wish"
-        case paymentBestBuyForm = "http://form.lvh.me:8080/payment-bestbuy"
-        case paymentTargetForm = "http://form.lvh.me:8080/payment-target"
-        case paymentKohlsAirbnbForm = "http://form.lvh.me:8080/payment-kohls-airbnb"
-        case paymentHotelsForm = "http://form.lvh.me:8080/payment-hotels"
-        case paymentExpediaForm = "http://form.lvh.me:8080/payment-expedia"
-        case paymentPricelineForm = "http://form.lvh.me:8080/payment-priceline"
-        case paymentHotwireForm = "http://form.lvh.me:8080/payment-hotwire"
-        case paymentKayakForm = "http://form.lvh.me:8080/payment-kayak"
-        case paymentAgodaForm = "http://form.lvh.me:8080/payment-agoda"
-        case paymentNetflixForm = "http://form.lvh.me:8080/payment-netflix"
-        case paymentDisneyForm = "http://form.lvh.me:8080/payment-disney"
-        case paymentHboMaxForm = "http://form.lvh.me:8080/payment-hbomax"
-        case paymentYoutubeForm = "http://form.lvh.me:8080/payment-youtube"
-        case paymentSpotifyForm = "http://form.lvh.me:8080/payment-spotify"
-        case paymentAppleForm = "http://form.lvh.me:8080/payment-apple"
-        case paymentUberForm = "http://form.lvh.me:8080/payment-uber"
-        case paymentDoordashForm = "http://form.lvh.me:8080/payment-doordash"
+    enum MockPageLink: CaseIterable {
+        case mainView
+        case mockBaseUrl
 
-        case visibilityForm = "http://form.lvh.me:8080/visibility"
-        
-        case socialMediaAdBlock = "http://social-media-adblock.test.adblock.lvh.me:8080/"
-        case insideAdBlock = "http://inside-adblock.test.adblock.lvh.me:8080/"
-        case fullSiteAdBlock = "http://a-stat.test.adblock.lvh.me:8080/"
-        case cookieBannerAdBlock = "http://annoyances-adblock.test.adblock.lvh.me:8080/"
-        case popupBannerAdBlock = "http://annoyances2-adblock.test.adblock.lvh.me:8080/"
-        case adsBannerAdBlock = "http://annoyances3-adblock.test.adblock.lvh.me:8080/"
-        
-        case newWindowBrowser = "http://windowopen.browser.lvh.me:8080/"
-        
-        case testPdfFile = "http://www.lvh.me:8080/static/test.pdf"
+        case ambiguousShortForm
+        case signupShortForm
+        case signinShortForm
+        case paymentNetflixShortForm
 
+        case signupForm
+        case signinForm
+        case signin1Form
+        case signin2Form
+        case signin3Form
+        case signin4Form
+        case signin5Form
+        case signin61Form
+        case signin71Form
+        case signin8Form
+        case signin91Form
+        case signin92Form
+        case signinstep2Form
+        case signinebayForm
+        case signup1Form
+        case signup2Form
+        case signup3Form
+        case signup4Form
+        case signup51Form
+        case signup6Form
+
+        case paymentForm
+        case paymentKiwiForm
+        case paymentOplataForm
+        case paymentBookingForm
+        case paymentSportDecouverteForm
+        case paymentAmazonForm
+        case paymentEbayForm
+        case paymentWalmartForm
+        case paymentEtsySearsForm
+        case paymentWishForm
+        case paymentBestBuyForm
+        case paymentTargetForm
+        case paymentKohlsAirbnbForm
+        case paymentHotelsForm
+        case paymentExpediaForm
+        case paymentPricelineForm
+        case paymentHotwireForm
+        case paymentKayakForm
+        case paymentAgodaForm
+        case paymentNetflixForm
+        case paymentDisneyForm
+        case paymentHboMaxForm
+        case paymentYoutubeForm
+        case paymentSpotifyForm
+        case paymentAppleForm
+        case paymentUberForm
+        case paymentDoordashForm
+
+        case visibilityForm
+
+        case socialMediaAdBlock
+        case insideAdBlock
+        case fullSiteAdBlock
+        case cookieBannerAdBlock
+        case popupBannerAdBlock
+        case adsBannerAdBlock
+
+        case newWindowBrowser
+
+        case testPdfFile
+
+        var rawValue: String {
+            let port = EnvironmentVariables.MockHttpServer.port
+            switch self {
+            case .mainView: return "http://localhost:\(port)/"
+            case .mockBaseUrl: return "http://form.lvh.me:\(port)/"
+
+            case .ambiguousShortForm: return "http://ambiguous.form.lvh.me:\(port)/"
+            case .signupShortForm: return "http://signup.form.lvh.me:\(port)/"
+            case .signinShortForm: return "http://signin.form.lvh.me:\(port)/"
+            case .paymentNetflixShortForm: return "http://payment-netflix.form.lvh.me:\(port)/"
+
+            case .signupForm: return "http://form.lvh.me:\(port)/signup"
+            case .signinForm: return "http://form.lvh.me:\(port)/signin"
+            case .signin1Form: return "http://form.lvh.me:\(port)/signinpage1"
+            case .signin2Form: return "http://form.lvh.me:\(port)/signinpage2"
+            case .signin3Form: return "http://form.lvh.me:\(port)/signinpage3"
+            case .signin4Form: return "http://form.lvh.me:\(port)/signinpage4"
+            case .signin5Form: return "http://form.lvh.me:\(port)/signinpage5"
+            case .signin61Form: return "http://form.lvh.me:\(port)/signinpage6-1"
+            case .signin71Form: return "http://form.lvh.me:\(port)/signinpage7-1"
+            case .signin8Form: return "http://form.lvh.me:\(port)/signinpage8"
+            case .signin91Form: return "http://form.lvh.me:\(port)/signinpage9-1"
+            case .signin92Form: return "http://form.lvh.me:\(port)/signinpage9-2"
+            case .signinstep2Form: return "http://form.lvh.me:\(port)/signinstep2"
+            case .signinebayForm: return "http://form.lvh.me:\(port)/signinebay"
+            case .signup1Form: return "http://form.lvh.me:\(port)/signuppage1"
+            case .signup2Form: return "http://form.lvh.me:\(port)/signuppage2"
+            case .signup3Form: return "http://form.lvh.me:\(port)/signuppage3"
+            case .signup4Form: return "http://form.lvh.me:\(port)/signuppage4"
+            case .signup51Form: return "http://form.lvh.me:\(port)/signuppage5-1"
+            case .signup6Form: return "http://form.lvh.me:\(port)/signuppage6"
+
+            case .paymentForm: return "http://form.lvh.me:\(port)/payment"
+            case .paymentKiwiForm: return "http://form.lvh.me:\(port)/payment-kiwi"
+            case .paymentOplataForm: return "http://form.lvh.me:\(port)/payment-oplata"
+            case .paymentBookingForm: return "http://form.lvh.me:\(port)/payment-booking"
+            case .paymentSportDecouverteForm: return "http://form.lvh.me:\(port)/payment-sport-decouverte"
+            case .paymentAmazonForm: return "http://form.lvh.me:\(port)/payment-amazon"
+            case .paymentEbayForm: return "http://form.lvh.me:\(port)/payment-ebay"
+            case .paymentWalmartForm: return "http://form.lvh.me:\(port)/payment-walmart"
+            case .paymentEtsySearsForm: return "http://form.lvh.me:\(port)/payment-etsy-sears"
+            case .paymentWishForm: return "http://form.lvh.me:\(port)/payment-wish"
+            case .paymentBestBuyForm: return "http://form.lvh.me:\(port)/payment-bestbuy"
+            case .paymentTargetForm: return "http://form.lvh.me:\(port)/payment-target"
+            case .paymentKohlsAirbnbForm: return "http://form.lvh.me:\(port)/payment-kohls-airbnb"
+            case .paymentHotelsForm: return "http://form.lvh.me:\(port)/payment-hotels"
+            case .paymentExpediaForm: return "http://form.lvh.me:\(port)/payment-expedia"
+            case .paymentPricelineForm: return "http://form.lvh.me:\(port)/payment-priceline"
+            case .paymentHotwireForm: return "http://form.lvh.me:\(port)/payment-hotwire"
+            case .paymentKayakForm: return "http://form.lvh.me:\(port)/payment-kayak"
+            case .paymentAgodaForm: return "http://form.lvh.me:\(port)/payment-agoda"
+            case .paymentNetflixForm: return "http://form.lvh.me:\(port)/payment-netflix"
+            case .paymentDisneyForm: return "http://form.lvh.me:\(port)/payment-disney"
+            case .paymentHboMaxForm: return "http://form.lvh.me:\(port)/payment-hbomax"
+            case .paymentYoutubeForm: return "http://form.lvh.me:\(port)/payment-youtube"
+            case .paymentSpotifyForm: return "http://form.lvh.me:\(port)/payment-spotify"
+            case .paymentAppleForm: return "http://form.lvh.me:\(port)/payment-apple"
+            case .paymentUberForm: return "http://form.lvh.me:\(port)/payment-uber"
+            case .paymentDoordashForm: return "http://form.lvh.me:\(port)/payment-doordash"
+
+            case .visibilityForm: return "http://form.lvh.me:\(port)/visibility"
+
+            case .socialMediaAdBlock: return "http://social-media-adblock.test.adblock.lvh.me:\(port)/"
+            case .insideAdBlock: return "http://inside-adblock.test.adblock.lvh.me:\(port)/"
+            case .fullSiteAdBlock: return "http://a-stat.test.adblock.lvh.me:\(port)/"
+            case .cookieBannerAdBlock: return "http://annoyances-adblock.test.adblock.lvh.me:\(port)/"
+            case .popupBannerAdBlock: return "http://annoyances2-adblock.test.adblock.lvh.me:\(port)/"
+            case .adsBannerAdBlock: return "http://annoyances3-adblock.test.adblock.lvh.me:\(port)/"
+
+            case .newWindowBrowser: return "http://windowopen.browser.lvh.me:\(port)/"
+
+            case .testPdfFile: return "http://www.lvh.me:\(port)/static/test.pdf"
+            }
+        }
     }
     
     @discardableResult
