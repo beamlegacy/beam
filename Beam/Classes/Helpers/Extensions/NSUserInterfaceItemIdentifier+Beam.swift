b4194710f12484f181e93b90cedd67985a20bc86
Adam Viaud <adam@beamapp.co>
2022-07-04 11:43:49 +0200

    Show original context menu for some right click invocations - BE-4596

--- Beam/Classes/Helpers/Extensions/NSUserInterfaceItemIdentifier+Beam.swift
+++ Beam/Classes/Helpers/Extensions/NSUserInterfaceItemIdentifier+Beam.swift
@@ -3,9 +3,14 @@ import AppKit
 extension NSUserInterfaceItemIdentifier {
     static let webKitTextLookUp = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierLookUp")
     static let webKitTextTranslate = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierTranslate")
+    static let webKitSearchWeb = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierSearchWeb")
 
     static let webKitCopyImage = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierCopyImage")
     static let webKitDownloadImage = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierDownloadImage")
+
+    static let webKitDownloadMedia = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierDownloadMedia")
+    static let webKitOpenMediaInNewWindow = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierOpenMediaInNewWindow")
+    static let webKitToggleEnhancedFullscreen = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierToggleEnhancedFullScreen")
 
     static let webKitSharing = NSUserInterfaceItemIdentifier("WKMenuItemIdentifierShareMenu")
 
