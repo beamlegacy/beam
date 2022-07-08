4526e0b4e676473956972bc3afb8f1d42d2dcf1d
quentin valero <quentin@beamapp.co>
2022-07-08 13:22:25 +0000

    Auto tests for right click menu on Links BE 4632

--- BeamUITests/Sources/Locators/RightClickMenuViewLocators.swift
+++ BeamUITests/Sources/Locators/RightClickMenuViewLocators.swift
@@ -9,24 +9,49 @@ import Foundation
 
 enum RightClickMenuViewLocators {
     
-    enum MenuItems: String, CaseIterable, UIElement {
+    enum ImageMenuItems: String, CaseIterable, UIElement {
         case openImageInNewTab = "Open Image in New Tab"
         case openImageInNewWindow = "Open Image in New Window"
         case saveToDownloads = "Save Image to \"Downloads\""
         case saveAs = "Save Image As..."
         case copyImageAddress = "Copy Image Address"
         case copyImage = "Copy Image"
-        case share = "WKMenuItemIdentifierShareMenu"
-        case inspectElement = "Inspect Element"
-
     }
     
-    enum ShareMenuItems: String, CaseIterable, UIElement {
+    enum LinkMenuItems: String, CaseIterable, UIElement {
+        case openLinkInNewTab = "Open Link in New Tab"
+        case openLinkInNewWindow = "Open Link in New Window"
+        case downloadLinkedFile = "Download Linked File"
+        case downloadLinkedFileAs = "Download Linked File As..."
+        case copyLink = "Copy Link"
+        case services = "Services"
+    }
+    
+    enum CommonMenuItems: String, CaseIterable, UIElement {
+        case share = "WKMenuItemIdentifierShareMenu"
+        case inspectElement = "Inspect Element"
+    }
+    
+    enum ShareCommonMenuItems: String, CaseIterable, UIElement {
         case shareByMail = "Mail"
         case shareByMessages = "Messages"
         case shareByNote = "Notes"
-        case shareByPhotos = "Add to Photos"
         case shareMoreOptions = "More…"
+    }
+    
+    enum ShareImageMenuItems: String, CaseIterable, UIElement {
+        case shareByPhotos = "Add to Photos"
+    }
+    
+    enum ServicesMenuItems: String, CaseIterable, UIElement {
+        case serviceMusic = "Add to Music as a Spoken Track"
+        case serviceOpen = "Open"
+        case serviceOpenManPage = "Open man Page in Terminal"
+        case serviceSearchManPage = "Search man Page Index in Terminal"
+        case serviceSearchGoogle = "Search With Google"
+        case serviceShowInFinder = "Show in Finder"
+        case serviceShowInfoInFinder = "Show Info in Finder"
+
 
     }
     
