9869a6fa0098a60a3f89148660152e9f5a14435b
Thomas Clement <thomas@beamapp.co>
2022-07-08 14:39:40 +0000

    Alert the user when entering an invalid url in the add blocker allow list, BE-4077

--- Beam/Classes/Views/Preferences/Privacy/AllowListViewModel.swift
+++ Beam/Classes/Views/Preferences/Privacy/AllowListViewModel.swift
@@ -11,25 +11,19 @@ import Combine
 
 class AllowListViewModel: ObservableObject {
 
-    @Published var allAllowListItems: [AllowListViewItem] = []
-    @Published var recentlyAddedItems: [AllowListViewItem] = []
-    private var subscribers = Set<AnyCancellable>()
+    @Published private(set) var allowListItems: [AllowListViewItem] = []
 
-    init() {
-        refreshAllAllowListItems()
-    }
+    private var currentAllowListItems: [AllowListViewItem] = []
+    private var subscribers = Set<AnyCancellable>()
 
     func refreshAllAllowListItems() {
         RadBlockDatabase.shared.allowlistEntryEnumerator(forGroup: nil, domain: nil, sortOrder: .createDate) { entries, error in
             if let entries = entries?.allObjects as? [RBAllowlistEntry] {
-                DispatchQueue.mainSync {
-                    self.allAllowListItems.removeAll()
-                }
-                for entry in entries {
-                    let item = AllowListViewItem(entry: entry)
-                    DispatchQueue.main.async {
-                        self.allAllowListItems.append(item)
-                    }
+                let listItems = entries.map { AllowListViewItem(entry: $0) }
+
+                DispatchQueue.main.async {
+                    self.currentAllowListItems = listItems
+                    self.allowListItems = listItems
                 }
             }
             if let error = error {
@@ -39,18 +33,41 @@ class AllowListViewModel: ObservableObject {
     }
 
     func add(domain: String) {
-        let item = AllowListViewItem(domain: domain)
-        DispatchQueue.main.async {
-            self.recentlyAddedItems.append(item)
-            self.allAllowListItems.append(item)
+        if let item = currentAllowListItems.first(where: { $0.host == domain }),
+           !allowListItems.contains(item) {
+            allowListItems.append(item)
+        } else {
+            let item = AllowListViewItem(domain: domain)
+            allowListItems.append(item)
         }
     }
 
+    func update(domain: String, at index: Int) {
+        allowListItems[index] = AllowListViewItem(domain: domain)
+    }
+
+    func remove(items: [AllowListViewItem]) {
+        allowListItems.removeAll(where: { items.contains($0) })
+    }
+
     func save() {
-        for item in recentlyAddedItems {
-            ContentBlockingManager.shared.radBlockPreferences.add(domain: item.host)
+        let current = Set(currentAllowListItems)
+        let new = Set(allowListItems)
+
+        let additions = new.subtracting(current)
+        let removals = current.subtracting(new)
+
+        // Process removals first, this ensures we do not lose anything in case we have equivalent items.
+        if !removals.isEmpty {
+            let entries = removals.compactMap { $0.entry }
+            ContentBlockingManager.shared.radBlockPreferences.remove(entries: entries, completion: { })
         }
-        self.recentlyAddedItems = []
-        refreshAllAllowListItems()
+        if !additions.isEmpty {
+            for item in additions {
+                ContentBlockingManager.shared.radBlockPreferences.add(domain: item.host)
+            }
+        }
+
+        currentAllowListItems = allowListItems
     }
 }
