72ced676cb1b1b35af28a07e959410cb1e0a98cc
Adam Viaud <adam@beamapp.co>
2022-07-11 22:37:06 +0000

    Harmonize TabGroup right click menu design - BE-4308

--- Beam/Classes/Helpers/Extensions/NSMenu+Beam.swift
+++ Beam/Classes/Helpers/Extensions/NSMenu+Beam.swift
@@ -1,6 +1,9 @@
 import AppKit
-import BeamCore
+import SwiftUI
 
+// MARK: - NSMenuItem subclasses
+
+/// A ``NSMenuItem`` subclass containing a closure for easy menus creation.
 final class HandlerMenuItem: NSMenuItem {
     typealias Handler = (NSMenuItem) -> Void
 
@@ -21,13 +24,37 @@ final class HandlerMenuItem: NSMenuItem {
     }
 }
 
-extension NSMenu {
+// MARK: - NSMenuItem components
 
+extension NSMenuItem {
+    /// A separator that will take the full width of the menu.
+    /// - Parameter height: the height of the separator, defaults to `1.0` point.
+    /// - Returns: A menu item that is used to separate logical groups of menu commands.
+    class func fullWidthSeparator(height: CGFloat = 1.0) -> NSMenuItem {
+        return ContentViewMenuItem(
+            title: "BeamFullWidthSeparator",
+            contentView: { Divider().background(BeamColor.Generic.macOSContextSeparator.swiftUI) },
+            insets: NSEdgeInsets(top: .zero, left: .zero, bottom: 5.0, right: .zero),
+            customization: { hostingView in
+                hostingView.heightAnchor.constraint(equalToConstant: height).isActive = true
+            }
+        )
+    }
+}
+
+// MARK: - NSMenu helpers
+
+extension NSMenu {
+    /// Adds a menu item to the end of the menu with a specified handler.
+    /// - Parameters:
+    ///   - title: A string to be made the title of the menu item.
+    ///   - keyEquivalent: A string identifying the key to use as a key equivalent for the menu item.
+    ///   - handler: The closure that will be executed when this item is selected.
+    /// - Returns: The created menu item.
     @discardableResult
     func addItem(withTitle title: String, keyEquivalent: String = "", handler: @escaping HandlerMenuItem.Handler) -> NSMenuItem {
         let item = HandlerMenuItem(title: title, keyEquivalent: keyEquivalent, handler: handler)
         addItem(item)
         return item
     }
-
 }
---
+++ Beam/Classes/Helpers/UI/ContentViewMenuItem.swift
@@ -0,0 +1,56 @@
+import AppKit
+import SwiftUI
+
+/// A ``NSMenuItem`` subclass taking a SwiftUI view as its content.
+final class ContentViewMenuItem<ContentView: View>: NSMenuItem {
+    typealias ContentBuilder = () -> ContentView
+    typealias CustomizationHandler = (NSHostingView<ContentView>) -> Void
+
+    private final class ContainerView: NSView {
+        override var acceptsFirstResponder: Bool {
+            return true
+        }
+    }
+
+    /// Constructs an item with a specified SwiftUI content view.
+    /// - Parameters:
+    ///   - title: The title of the menu item.
+    ///   - keyEquivalent: A string representing a keyboard key to be used as the key equivalent.
+    ///   - contentView: A ``SwiftUI/ViewBuilder`` that produces the view.
+    ///   - insets: A ``NSEdgeInsets`` for the content, defaults to `.zero`.
+    ///   - customization: A closure allowing to further customize the view hosting the SwiftUI view.
+    ///
+    ///   You can use the ``customization`` closure to further constraints the container view, since
+    ///   SwiftUI might not communicate its intrinsic size.
+    init(
+        title: String,
+        keyEquivalent: String = "",
+        @ViewBuilder contentView: @escaping ContentBuilder,
+        insets: NSEdgeInsets = .init(top: .zero, left: .zero, bottom: .zero, right: .zero),
+        customization: CustomizationHandler? = nil
+    ) {
+        super.init(title: title, action: nil, keyEquivalent: keyEquivalent)
+
+        let hostingView = NSHostingView<ContentView>(rootView: contentView())
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+
+        let container = ContainerView(frame: .zero)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.addSubview(hostingView)
+
+        NSLayoutConstraint.activate([
+            hostingView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: insets.left),
+            hostingView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -insets.right),
+            hostingView.topAnchor.constraint(equalTo: container.topAnchor, constant: insets.top),
+            hostingView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -insets.bottom)
+        ])
+
+        self.view = container
+
+        customization?(hostingView)
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
