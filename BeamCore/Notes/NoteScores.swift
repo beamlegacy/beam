26279c1c6a29ea1fcd82ababdf5d0129f7ac0c76
Paul Lefkopoulos <paul@beamapp.co>
2022-07-11 20:13:29 +0000

    Fix disappearing notes in daily summary/continue to BE-4548

--- BeamCore/Notes/NoteScores.swift
+++ BeamCore/Notes/NoteScores.swift
@@ -12,7 +12,7 @@ public class NoteScore: Codable {
     var minWordCount: Int?
     var maxWordCount: Int = 0
     var firstWordCount: Int?
-    var lastWordCount: Int = 0
+    public var lastWordCount: Int = 0
     public var addedBidiLinkToCount: Int = 0
     public var captureToCount: Int = 0
     public var visitCount: Int = 0
@@ -34,12 +34,26 @@ public class NoteScore: Codable {
             + log(1 + abs(Float(firstToLastDeltaWordCount)))
     }
 }
+public struct NoteLastWordCountChangeDay: Codable {
+    public let noteId: UUID
+    public let lastChangeDay: String?
+    public let lastWordCount: Int
+
+    public init(noteId: UUID, lastChangeDay: String?, lastWordCount: Int) {
+        self.noteId = noteId
+        self.lastChangeDay = lastChangeDay
+        self.lastWordCount = lastWordCount
+    }
+}
 
 public typealias NoteScoresById = [UUID: NoteScore]
 public typealias DailyNoteScores = [String: NoteScoresById]
+public typealias NotesLastWordCountChangeDay = [UUID: NoteLastWordCountChangeDay]
 
 public protocol DailyNoteScoreStoreProtocol {
     func apply(to noteId: UUID, changes: (NoteScore) -> Void)
+    func recordLastWordCountChange(noteId: UUID, wordCount: Int)
+    func getNoteIdsLastChangedAtAndAfter(daysAgo: Int) -> (Set<UUID>, Set<UUID>)
     func cleanup(daysToKeep: Int)
     func getScore(noteId: UUID, daysAgo: Int) -> NoteScore?
     func getScores(daysAgo: Int) -> [UUID: NoteScore]
@@ -48,6 +62,7 @@ public protocol DailyNoteScoreStoreProtocol {
 
 open class InMemoryDailyNoteScoreStore: DailyNoteScoreStoreProtocol {
     public var scores = DailyNoteScores()
+    public var notesLastWordCountChangeDay = NotesLastWordCountChangeDay()
     public static var backgroundQueue: DispatchQueue = DispatchQueue(label: "InMemoryDailyNoteScoreStore backgroundQueue")
 
     public init() {}
@@ -61,6 +76,32 @@ open class InMemoryDailyNoteScoreStore: DailyNoteScoreStoreProtocol {
             self.scores[localDay] = dayScores
         }
     }
+
+    public func recordLastWordCountChange(noteId: UUID, wordCount: Int) {
+        guard let localDay = BeamDate.now.localDayString() else { return }
+        Self.backgroundQueue.sync {
+            if let lastWordCount = notesLastWordCountChangeDay[noteId]?.lastWordCount,
+               lastWordCount != wordCount {
+                notesLastWordCountChangeDay[noteId] = NoteLastWordCountChangeDay(noteId: noteId, lastChangeDay: localDay, lastWordCount: wordCount)
+                return
+            }
+            if notesLastWordCountChangeDay[noteId] == nil {
+                notesLastWordCountChangeDay[noteId] = NoteLastWordCountChangeDay(noteId: noteId, lastChangeDay: nil, lastWordCount: wordCount)
+            }
+        }
+    }
+
+    public func getNoteIdsLastChangedAtAndAfter(daysAgo: Int) -> (Set<UUID>, Set<UUID>)  {
+        let day = Calendar(identifier: .iso8601).date(byAdding: .day, value: -daysAgo, to: BeamDate.now)?.localDayString()
+        guard let day = day else { return (Set<UUID>(), Set<UUID>()) }
+        return Self.backgroundQueue.sync {
+            return (
+                Set( (notesLastWordCountChangeDay.filter { $0.value.lastChangeDay == day}).keys),
+                Set( (notesLastWordCountChangeDay.filter { $0.value.lastChangeDay ?? "0000-00-00" > day}).keys)
+            )
+        }
+    }
+
     public func cleanup(daysToKeep: Int) {
         let existingDays =  scores.keys
         let daysToKeep = daysToKeep
@@ -71,6 +112,7 @@ open class InMemoryDailyNoteScoreStore: DailyNoteScoreStoreProtocol {
             }
         }
     }
+
     public func getScore(noteId: UUID, daysAgo: Int) -> NoteScore? {
         let day = Calendar(identifier: .iso8601).date(byAdding: .day, value: -daysAgo, to: BeamDate.now)?.localDayString()
         guard let day = day else { return nil }
@@ -78,6 +120,7 @@ open class InMemoryDailyNoteScoreStore: DailyNoteScoreStoreProtocol {
             scores[day]?[noteId]
         }
     }
+
     public func getScores(daysAgo: Int) -> [UUID: NoteScore] {
         let day = Calendar(identifier: .iso8601).date(byAdding: .day, value: -daysAgo, to: BeamDate.now)?.localDayString()
         guard let day = day else { return  [UUID: NoteScore]() }
@@ -85,6 +128,7 @@ open class InMemoryDailyNoteScoreStore: DailyNoteScoreStoreProtocol {
             scores[day] ?? [UUID: NoteScore]()
         }
     }
+
     public func clear() {
         Self.backgroundQueue.sync {
             scores = DailyNoteScores()
@@ -98,6 +142,7 @@ public class NoteScorer {
     public static var shared = NoteScorer(dailyStorage: InMemoryDailyNoteScoreStore())
 
     let dailyStorage: DailyNoteScoreStoreProtocol
+
     public init(dailyStorage: DailyNoteScoreStoreProtocol = InMemoryDailyNoteScoreStore()) {
         self.dailyStorage = dailyStorage
     }
@@ -105,6 +150,7 @@ public class NoteScorer {
     private func updateLocalDaily(noteId: UUID, changes: (NoteScore) -> Void) {
         dailyStorage.apply(to: noteId, changes: changes)
     }
+
     func updateWordCount(noteId: UUID, wordCount: Int) {
         updateLocalDaily(noteId: noteId) {
             if let minWordCount = $0.minWordCount {
@@ -114,26 +160,36 @@ public class NoteScorer {
             $0.firstWordCount = $0.firstWordCount ?? wordCount
             $0.lastWordCount = wordCount
         }
+        dailyStorage.recordLastWordCountChange(noteId: noteId, wordCount: wordCount)
     }
+
+    public func getNoteIdsLastChangedAtAndAfter(daysAgo: Int = 1)  -> (Set<UUID>, Set<UUID>) {
+        dailyStorage.getNoteIdsLastChangedAtAndAfter(daysAgo: daysAgo)
+    }
+
     public func incrementBidiLinkToCount(noteId: UUID) {
         updateLocalDaily(noteId: noteId) {
             $0.addedBidiLinkToCount += 1
         }
     }
+
     public func incrementCaptureToCount(noteId: UUID) {
         updateLocalDaily(noteId: noteId) {
             $0.captureToCount += 1
         }
     }
+
     public func incrementVisitCount(noteId: UUID) {
         updateLocalDaily(noteId: noteId) {
             $0.visitCount += 1
         }
     }
+
     public func cleanup(daysToKeep: Int? = nil) {
         let daysToKeep = daysToKeep ?? Self.daysToKeep
         dailyStorage.cleanup(daysToKeep: daysToKeep)
     }
+
     func getLocalDailyScore(noteId: UUID, daysAgo: Int = 1) -> NoteScore? {
         return dailyStorage.getScore(noteId: noteId, daysAgo: daysAgo)
     }
