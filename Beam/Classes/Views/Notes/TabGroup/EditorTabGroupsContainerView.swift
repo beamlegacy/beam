f96c4eb23983bc84ae6409d9b26a3d283f638268
Ludovic Ollagnier <ludovic@ollagnier.me>
2022-07-15 10:00:40 +0000

    Tab group in editor design review, BE-4768

--- Beam/Classes/Views/Notes/TabGroup/EditorTabGroupsContainerView.swift
+++ Beam/Classes/Views/Notes/TabGroup/EditorTabGroupsContainerView.swift
@@ -13,21 +13,26 @@ struct EditorTabGroupsContainerView: View {
     let tabGroups: [TabGroupBeamObject]
     let note: BeamNote
 
-    @State var hoveredTab: TabGroupBeamObject.PageInfo?
-    @State var hoveredTabGroupFrame: CGPoint?
+    @State private var hoveredTab: TabGroupBeamObject.PageInfo?
+    @State private var hoveredTabGroupFrame: CGPoint?
+    @State private var hoveredTabGroupColor: Color?
 
     let columns = [
-        GridItem(.adaptive(minimum: 100, maximum: 200)),
-        GridItem(.adaptive(minimum: 100, maximum: 200)),
-        GridItem(.adaptive(minimum: 100, maximum: 200)),
-        GridItem(.adaptive(minimum: 100, maximum: 200))
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+        GridItem(.flexible(minimum: 100, maximum: 200))
     ]
 
     var body: some View {
         GeometryReader { proxy in
-            LazyVGrid(columns: columns, spacing: 10) {
+            LazyVGrid(columns: columns, alignment: .leading, spacing: 10) {
                 ForEach(tabGroups) { group in
-                    EditorTabGroupView(tabGroup: group, note: note, hoveredTab: $hoveredTab, hoveredGroupFrame: $hoveredTabGroupFrame)
+                    EditorTabGroupView(tabGroup: group,
+                                       note: note,
+                                       hoveredTab: $hoveredTab,
+                                       hoveredGroupFrame: $hoveredTabGroupFrame,
+                                       hoveredGroupColor: $hoveredTabGroupColor)
                 }.zIndex(0)
             }
             .overlay(previewOverlay(for: hoveredTab, localProxy: proxy.frame(in: .global)), alignment: .top)
@@ -35,14 +40,16 @@ struct EditorTabGroupsContainerView: View {
     }
 
     @ViewBuilder private func previewOverlay(for tab: TabGroupBeamObject.PageInfo?, localProxy: CGRect) -> some View {
-        if let hoveredTab = hoveredTab, let hoveredTabGroupFrame = hoveredTabGroupFrame {
-            TabPreview(tab: hoveredTab)
-                .position(CGPoint(x: hoveredTabGroupFrame.x - localProxy.origin.x , y: hoveredTabGroupFrame.y - localProxy.origin.y + offset(for: hoveredTab)))
+        if let hoveredTab = hoveredTab,
+           let hoveredTabGroupFrame = hoveredTabGroupFrame,
+           let groupColor = hoveredTabGroupColor {
+            TabPreview(tab: hoveredTab, placeholderTintColor: groupColor)
+                .position(CGPoint(x: hoveredTabGroupFrame.x - localProxy.origin.x, y: hoveredTabGroupFrame.y - localProxy.origin.y + previewOffset))
         }
     }
 
-    private func offset(for pageInfo: TabGroupBeamObject.PageInfo) -> CGFloat {
-        let previewHeight = pageInfo.snapshot == nil ? 36.0 : 154.0
+    private var previewOffset: CGFloat {
+        let previewHeight =  154.0
         let tabGroupViewHeight = EditorTabGroupView.height
         let margin = 4.0
         return (previewHeight / 2 + tabGroupViewHeight / 2) + margin
