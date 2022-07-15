3745693056843155a7b1050d7fbc7cf909f3e7b7
Andrii Vasyliev <andrii@beamapp.co>
2022-07-15 18:59:27 +0000

    Start beam with opened tabs preferences UI test - BE-4770

---
+++ BeamUITests/E2ETests/Preferences/General/GeneralPreferencesTests.swift
@@ -0,0 +1,76 @@
+//
+//  GeneralPreferencesTests.swift
+//  BeamUITests
+//
+//  Created by Andrii Vasyliev on 14.07.2022.
+//
+
+import Foundation
+import XCTest
+
+class GeneralPreferencesTests: BaseTest {
+    
+    let generalPrefView = GeneralPreferenceTestView()
+    let basePrefView = PreferencesBaseView()
+    let browserPrefView = BrowserPreferencesTestView()
+    var journalView: JournalTestView!
+    var startBeamCheckbox: XCUIElement!
+    
+    override func setUp() {
+        journalView = launchApp()
+        openGeneralPrefs()
+        startBeamCheckbox = generalPrefView.getStartBeamWithOpenedTabsElement()
+    }
+    
+    private func openGeneralPrefs() {
+        shortcutHelper.shortcutActionInvoke(action: .openPreferences)
+        basePrefView.navigateTo(preferenceView: .general)
+    }
+    
+    func testStartBeamWithOpenedTabs() {
+        
+        step("THEN I see Start beam with opened tabs checkbox available") {
+            XCTAssertTrue(startBeamCheckbox.waitForExistence(timeout: BaseTest.minimumWaitTimeout))
+            XCTAssertTrue(generalPrefView.staticText(GeneralPreferencesViewLocators.StaticTexts.startBeamlabel.accessibilityIdentifier).exists)
+        }
+        
+        step("WHEN Start beam with opened tabs and Restore all tabs from last session checkboxes are enabled") {
+            if !startBeamCheckbox.isSettingEnabled() {
+                startBeamCheckbox.tapInTheMiddle()
+            }
+            basePrefView.navigateTo(preferenceView: .browser)
+            if !browserPrefView.getRestoreTabsCheckbox().isSettingEnabled() {
+                browserPrefView.getRestoreTabsCheckbox().tapInTheMiddle()
+            }
+            shortcutHelper.shortcutActionInvoke(action: .close)
+        }
+        
+        step("THEN Webview is opened on restart with openned tab") {
+            uiMenu.loadUITestPage1()
+            self.restartApp()
+            XCTAssertTrue(webView.waitForWebViewToLoad())
+            XCTAssertEqual(webView.getNumberOfTabs(), 1)
+        }
+
+        if !isBigSurOS() {
+            step("THEN Webview is opened on restart with pinned tab") {
+                webView.openTabMenu(tabIndex: 0).selectTabMenuItem(.pinTab)
+                self.restartApp()
+                XCTAssertTrue(webView.waitForWebViewToLoad())
+                XCTAssertEqual(webView.getNumberOfPinnedTabs(), 1)
+            }
+        }
+        
+        step("WHEN I disable Start beam with opened tabs checkbox") {
+            openGeneralPrefs()
+            startBeamCheckbox.clickOnExistence()
+        }
+        
+        step("THEN Journal view is opened on restart") {
+            self.restartApp()
+            XCTAssertTrue(journalView
+                            .waitForJournalViewToLoad()
+                            .isJournalOpened())
+        }
+    }
+}
