5a430213ab67f8bf3634c16c26581fe48105bae0
Jerome Blondon <jbl@beamapp.co>
2022-07-15 09:26:08 +0000

    Fix initial sync fail manager not loaded BE-4774

---
+++ BeamTests/DocumentStorage/BeamAccount+InitialStateTest.swift
@@ -0,0 +1,85 @@
+//
+//  BeamAccount+InitialState.swift
+//  BeamTests
+//
+//  Created by Jérôme Blondon on 13/07/2022.
+//
+
+import Foundation
+import XCTest
+@testable import BeamCore
+@testable import Beam
+
+import Nimble
+import GRDB
+
+class BeamAccountInitialStateTest: XCTestCase, BeamDocumentSource {
+    static var sourceId: String { "\(Self.self)" }
+
+    override func setUpWithError() throws {
+        BeamDate.freeze("2021-03-19T12:21:03Z")
+
+        logout()
+        try setupInitialState()
+    }
+
+    override func tearDown() {
+        logout()
+    }
+
+    func testAfterSignupLater() throws {
+        // we have the good number of managers
+        XCTAssertEqual(BeamObjectManager.managerOrder.count, BeamObjectManager.managerInstances.count, "Wrong number of registered managers!")
+
+        // we have the daily note
+        guard BeamNote.fetch(journalDate: BeamDate.now) != nil else {
+            XCTFail("Cannot get journal day")
+            return
+        }
+
+        // the previous daily note
+        let yesterday = BeamDate.now.addingTimeInterval(-60 * 60 * 24)
+        guard BeamNote.fetch(journalDate: yesterday) != nil else {
+            XCTFail("Cannot get journal day")
+            return
+        }
+
+        // and onboarding notes
+        guard BeamNote.fetch(title: "How to beam") != nil else {
+            XCTFail("Cannot get 'How to beam' note")
+            return
+        }
+        guard BeamNote.fetch(title: "Capture") != nil else {
+            XCTFail("Cannot get 'Capture")
+            return
+        }
+
+        // and 2 frecencies for journal
+        guard let noteLinksAndRefManager = BeamData.shared.currentDatabase?.noteLinksAndRefsManager else {
+            XCTFail("Cannot get noteLinksAndRefsManager")
+            return
+        }
+        let frecencyNoteRecords = try noteLinksAndRefManager.allNoteFrecencies(updatedSince: nil)
+        XCTAssertEqual(frecencyNoteRecords.count, 2)
+    }
+
+    private func logout() {
+        for window in AppDelegate.main.windows {
+            window.state.closeAllTabs(closePinnedTabs: true)
+        }
+        BeamData.shared.currentAccount?.logout()
+        AppDelegate.main.deleteAllLocalData()
+    }
+
+    private func setupInitialState() throws {
+        guard let noteLinksAndRefManager = BeamData.shared.currentDatabase?.noteLinksAndRefsManager else {
+            XCTFail("Cannot get noteLinksAndRefsManager")
+            return
+        }
+        try noteLinksAndRefManager.clear()
+
+        OnboardingNoteCreator.shared.createOnboardingNotes()
+        try BeamData.shared.reloadJournal()
+    }
+}
+
