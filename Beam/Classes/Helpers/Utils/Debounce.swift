2f5e8a53db9ab83e5571da9a26a073317e5608e2
Remi Santos <remi@beamapp.co>
2022-07-26 08:40:34 +0000

    Recalculate tabs draggable area when group item is renamed BE-4776

--- Beam/Classes/Helpers/Utils/Debounce.swift
+++ Beam/Classes/Helpers/Utils/Debounce.swift
@@ -1,13 +1,10 @@
 //
 //  Debounce.swift
 //  Beam
-//
-//  Created by Simon Ljungberg on 19/12/16.
-//  License: MIT
-//
 
 import Foundation
 import Combine
+import SwiftUI
 
 /**
  Wraps a function in a new function that will only execute the wrapped function if `delay` has passed without this function being called.
@@ -17,6 +14,9 @@ import Combine
  - Parameter action: A function to debounce. Can't accept any arguments.
 
  - Returns: A new function that will only call `action` if `delay` time passes between invocations.
+
+ Created by Simon Ljungberg on 19/12/16.
+ License: MIT
  */
 func debounce(delay: DispatchTimeInterval, queue: DispatchQueue = .main, action: @escaping (() -> Void)) -> () -> Void {
     var currentWorkItem: DispatchWorkItem?
@@ -42,4 +42,18 @@ func debouncePublisher(delay: DispatchTimeInterval) -> AnyPublisher<Any, Never> 
             promise(.success(true))
         }
     }.eraseToAnyPublisher()
+}
+
+extension View {
+    /**
+     Wrapper for View.onPreferenceChange() that will perform the action only if a delay has passed since the last preference change was received.
+     */
+    @inlinable public func onPreferenceChangeDebounced<K>(_ key: K.Type = K.self, delay: DispatchTimeInterval, queue: DispatchQueue = .main, perform action: @escaping (K.Value) -> Void) -> some View where K : PreferenceKey, K.Value : Equatable {
+        var currentWorkItem: DispatchWorkItem?
+        return self.onPreferenceChange(key) { v in
+            currentWorkItem?.cancel()
+            currentWorkItem = DispatchWorkItem { action(v) }
+            queue.asyncAfter(deadline: .now() + delay, execute: currentWorkItem!)
+        }
+    }
 }
