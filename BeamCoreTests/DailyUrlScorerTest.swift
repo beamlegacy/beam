bfdebdcabb27175832114d24d04795d8c58ab60e
Paul Lefkopoulos <paul@beamapp.co>
2022-08-22 16:37:05 +0000

    Allow free time frame in advanced prefs url stats export BE-4844

--- BeamCoreTests/DailyUrlScorerTest.swift
+++ BeamCoreTests/DailyUrlScorerTest.swift
@@ -9,12 +9,13 @@ import XCTest
 @testable import BeamCore
 
 private class FakeDailyScoreStore: DailyUrlScoreStoreProtocol {
-    let scores: [UUID: DailyURLScore]
-    init(scores: [UUID: DailyURLScore], repeatedUrls: Set<String> = Set<String>()) {
+    let scores: [UUID: AggregatedURLScore]
+    init(scores: [UUID: AggregatedURLScore], repeatedUrls: Set<String> = Set<String>()) {
         self.scores = scores
     }
     func apply(to urlId: UUID, changes: (DailyURLScore) -> Void) {}
-    func getScores(daysAgo: Int) -> [UUID: DailyURLScore] { scores }
+    func getScores(daysAgo: Int) -> [UUID: DailyURLScore] { [UUID: DailyURLScore]() }
+    func getAggregatedScores(between offset0: Int, and offset1: Int) -> [UUID: AggregatedURLScore] { scores }
     func getDailyRepeatingUrlsWithoutFragment(between offset0: Int, and offset1: Int, minRepeat: Int) -> Set<String> { Set<String>() }
     func getUrlWithoutFragmentDistinctVisitDayCount(between offset0: Int, and offset1: Int) -> [String: Int] { [String: Int]() }
 }
@@ -94,29 +95,31 @@ class DailyUrlScorerTest: XCTestCase {
             URL(string: "http://abc.fr")!: [urlIds[0], urlIds[1]],
             URL(string: "http://def.fr")!: [urlIds[2]]
         ]
-        let score0 = DailyURLScore(urlId: urlIds[0], localDay: "2020-01-01")
-        score0.visitCount = 1
-        score0.scrollRatioX = 0.2
-        score0.scrollRatioY = 0.5
-        score0.isPinned = true
-        score0.textSelections = 1
-        score0.textAmount = 2
-        score0.readingTimeToLastEvent = 1.0
-        score0.area = 1
-        score0.navigationCountSinceLastSearch = 4
-        let score1 = DailyURLScore(urlId: urlIds[1], localDay: "2020-01-01")
-        score1.visitCount = 1
-        score1.scrollRatioX = 0.5
-        score1.scrollRatioY = 0.2
-        score1.isPinned = false
-        score1.textSelections = 1
-        score1.textAmount = 3
-        score1.readingTimeToLastEvent = 1.0
-        score1.area = 2
-        score1.navigationCountSinceLastSearch = 2
-        let score2 = DailyURLScore(urlId: urlIds[2], localDay: "2020-01-01")
-        score2.visitCount = 2
-
+        let score0 = AggregatedURLScore(
+            visitCount: 1,
+            readingTimeToLastEvent: 1.0,
+            textSelections: 1,
+            scrollRatioX: 0.2,
+            scrollRatioY: 0.5,
+            textAmount: 2,
+            area: 1,
+            isPinned: true,
+            navigationCountSinceLastSearch: 4
+        )
+        let score1 = AggregatedURLScore(
+            visitCount: 1,
+            readingTimeToLastEvent: 1.0,
+            textSelections: 1,
+            scrollRatioX: 0.5,
+            scrollRatioY: 0.2,
+            textAmount: 3,
+            area: 2,
+            isPinned: false,
+            navigationCountSinceLastSearch: 2
+        )
+        let score2 = AggregatedURLScore(
+            visitCount: 2
+        )
         let scores = [
             urlIds[0]: score0,
             urlIds[1]: score1,
@@ -187,44 +190,44 @@ class DailyUrlScorerTest: XCTestCase {
             BeamDate.travel(1)
             return LinkStore.shared.visit($0.0, title: $0.1, content: nil, destination: nil).id
         }
-        let score0 = DailyURLScore(urlId: urlIds[0], localDay: "2020-01-01")
-        let score1 = DailyURLScore(urlId: urlIds[1], localDay: "2020-01-01")
-        let score2 = DailyURLScore(urlId: urlIds[2], localDay: "2020-01-01")
-        let score3 = DailyURLScore(urlId: urlIds[3], localDay: "2020-01-01")
-        let score4 = DailyURLScore(urlId: urlIds[4], localDay: "2020-01-01")
-        let score5 = DailyURLScore(urlId: urlIds[5], localDay: "2020-01-01")
-        let score6 = DailyURLScore(urlId: urlIds[6], localDay: "2020-01-01")
-        let score7 = DailyURLScore(urlId: urlIds[7], localDay: "2020-01-01")
-        let score8 = DailyURLScore(urlId: urlIds[8], localDay: "2020-01-01")
-
-        score0.readingTimeToLastEvent = 1.5
-        score0.textAmount = 500
-
-        score1.readingTimeToLastEvent = 1.5
-        score1.textAmount = 500
-
-        score2.readingTimeToLastEvent = 2
-        score2.textAmount = 500
-
-        score3.readingTimeToLastEvent = 10
-        score3.isPinned = true
-        score3.textAmount = 500
-
-        score4.readingTimeToLastEvent = 100
-        score4.textAmount = 500
-
-        score5.readingTimeToLastEvent = 0.5
-        score5.textAmount = 10_000
-
-        score6.readingTimeToLastEvent = 10
-        score6.textAmount = 500
-
-        score7.readingTimeToLastEvent = 10
-        score7.textAmount = 500
-        score7.textSelections = 1
-
-        score8.readingTimeToLastEvent = 10_000
-        score8.textAmount = 499
+        let score0 = AggregatedURLScore(
+            readingTimeToLastEvent: 1.5,
+            textAmount: 500
+        )
+        let score1 = AggregatedURLScore(
+            readingTimeToLastEvent: 1.5,
+            textAmount: 500
+        )
+        let score2 = AggregatedURLScore(
+            readingTimeToLastEvent: 2,
+            textAmount: 500
+        )
+        let score3 = AggregatedURLScore(
+            readingTimeToLastEvent: 10,
+            textAmount: 500,
+            isPinned: true
+        )
+        let score4 = AggregatedURLScore(
+            readingTimeToLastEvent: 100,
+            textAmount: 500
+        )
+        let score5 = AggregatedURLScore(
+            readingTimeToLastEvent: 0.5,
+            textAmount: 10_000
+        )
+        let score6 = AggregatedURLScore(
+            readingTimeToLastEvent: 10,
+            textAmount: 500
+        )
+        let score7 = AggregatedURLScore(
+            readingTimeToLastEvent: 10,
+            textSelections: 1,
+            textAmount: 500
+        )
+        let score8 = AggregatedURLScore(
+            readingTimeToLastEvent: 10_000,
+            textAmount: 499
+        )
 
         let scores = [
             urlIds[0]: score0,
@@ -239,7 +242,7 @@ class DailyUrlScorerTest: XCTestCase {
         ]
         let params = DailySummaryUrlParams(minReadingTime: 1, minTextAmount: 500, maxRepeatTimeFrame: 0, maxRepeat: 0)
         let scorer = DailyUrlScorer(store: FakeDailyScoreStore(scores: scores), params: params)
-        let scoredUrls = scorer.getHighScoredUrls(daysAgo: 0, topN: 1)
+        let scoredUrls = scorer.getHighScoredUrls(between: 0, and: 0, topN: 1)
         XCTAssertEqual(scoredUrls.count, 1)
         //the 2 first url scores got aggregated and title was chosen as most recent.
         XCTAssertEqual(scoredUrls[0].url.absoluteString, "https://abc.com/page1")
