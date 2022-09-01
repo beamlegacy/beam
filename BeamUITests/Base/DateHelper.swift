9067ec8405ff4c60aa3bc2b8a362832cad62d529
Andrii Vasyliev <andrii@beamapp.co>
2022-09-01 12:34:24 +0000

    UI test for BiDi creation using daily notes keywords - BE-5184

--- BeamUITests/Base/DateHelper.swift
+++ BeamUITests/Base/DateHelper.swift
@@ -16,6 +16,14 @@ class DateHelper {
         return dateFormatter.string(from: BeamDate.now)
     }
     
+    func getDateString(daysDifferenceFromToday: Int, _ format: DateFormats) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format.rawValue
+        let calendar = Calendar(identifier: .iso8601)
+        let requestedDay = calendar.date(byAdding: .day, value: daysDifferenceFromToday, to: BeamDate.now) ?? BeamDate.now
+        return dateFormatter.string(from: requestedDay)
+    }
+    
     enum DateFormats: String {
         case noteViewTitle = "d MMMM yyyy"
         case noteViewCreation = "MMMM dd, yyyy"
