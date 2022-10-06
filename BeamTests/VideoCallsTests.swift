4f51c462f152bc7c95ed8463104db16a9a9a5ac1
Adam Viaud <adam@beamapp.co>
2022-10-06 14:24:47 +0000

    Add FaceTime support for meeting links and video call side window - BE-4590

--- BeamTests/VideoCallsTests.swift
+++ BeamTests/VideoCallsTests.swift
@@ -23,11 +23,13 @@ final class VideoCallsTests: XCTestCase {
         XCTAssertTrue(manager.isEligible(url: try XCTUnwrap(URL(string: "http://meet.google.com/nqz-vnxj-xrg"))))
         XCTAssertTrue(manager.isEligible(url: try XCTUnwrap(URL(string: "https://beamapp.zoom.us/wc/join/12345678910"))))
         XCTAssertTrue(manager.isEligible(url: try XCTUnwrap(URL(string: "http://beamapp.zoom.us/wc/join/12345678910"))))
+        XCTAssertTrue(manager.isEligible(url: try XCTUnwrap(URL(string: "https://facetime.apple.com/join#v=1&p=hDlLUz/TEe2lcgJC+hMtRw&k=YF2Y7sgNhTTrGR8H1_8dhLhhCtmLnPHqbmJTTsgPMpM"))))
 
         XCTAssertFalse(manager.isEligible(url: try XCTUnwrap(URL(string: "https://calendar.google.com/"))))
         XCTAssertFalse(manager.isEligible(url: try XCTUnwrap(URL(string: "https://www.google.com/search?q=beamapp"))))
         XCTAssertFalse(manager.isEligible(url: try XCTUnwrap(URL(string: "https://zoom.us"))))
-        XCTAssertFalse(manager.isEligible(url: try XCTUnwrap(URL(string: "https://facetime.apple.com/join#v=1&p=hDlLUz/TEe2lcgJC+hMtRw&k=YF2Y7sgNhTTrGR8H1_8dhLhhCtmLnPHqbmJTTsgPMpM"))))
+        XCTAssertFalse(manager.isEligible(url: try XCTUnwrap(URL(string: "https://facetime.apple.com/"))))
+
     }
 
 }
